98666c8f9268e9ae70a247fc2b0e0a9345843a9d
Kittinun Vantasin <kittinunf@kittinunf.local>
2015-05-13 16:29:52 +0700

    inception of Fuel

---
+++ app/src/main/kotlin/com/example/kotlin/fueldemo/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.kotlin.fueldemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import fuel.Fuel
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val str = Fuel().greet("Octto")
+        Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item!!.getItemId()
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
---
+++ fuel/src/main/kotlin/fuel/Fuel.kt
@@ -0,0 +1,9 @@
+package fuel
+
+/**
+ * Created by Kittinun Vantasin on 5/13/15.
+ */
+
+class Fuel {
+
+}
