31906d2777eb8a3c5ea1cbc790051c847ff92018
Kittinun Vantasin <kittinun.f@gmail.com>
2015-07-23 15:48:27 +0700

    bring back the sample

--- sample/src/main/kotlin/com/example/fuel/MainActivity.kt
+++ sample/src/main/kotlin/com/example/fuel/MainActivity.kt
@@ -29,65 +29,53 @@ public class MainActivity : AppCompatActivity() {
     }
 
     fun execute() {
-//        Manager.sharedInstance.basePath = "http://httpbin.org"
-//        Manager.sharedInstance.additionalHeaders = mapOf("Device" to "Android")
-//        Manager.sharedInstance.additionalParams = mapOf("key" to "value")
-//
-//        Fuel.get("/get", mapOf("foo" to "foo", "bar" to "bar")).responseString { request, response, either ->
-//            updateUI(response, either)
-//        }
-//
-//        Fuel.post("/post", mapOf("foo" to "foo", "bar" to "bar")).responseString { request, response, either ->
-//            updateUI(response, either)
-//        }
-//
-//        Fuel.put("/put", mapOf("foo" to "foo", "bar" to "bar")).responseString { request, response, either ->
-//            updateUI(response, either)
-//        }
-//
-//        Fuel.delete("/delete", mapOf("foo" to "foo", "bar" to "bar")).responseString { request, response, either ->
-//            updateUI(response, either)
-//        }
-//
-//        Fuel.download("/bytes/1048").destination { response, url ->
-//            val sd = Environment.getExternalStorageDirectory();
-//            val location = File(sd.getAbsolutePath() + "/test")
-//            location.mkdir()
-//            File(location, "test.tmp")
-//        }.progress { readBytes, totalBytes ->
-//            Log.e(TAG, "download: ${readBytes.toFloat() / totalBytes.toFloat()}")
-//        }.responseString { request, response, either ->
-//            updateUI(response, either)
-//        }
-//
-//        val username = "username"
-//        val password = "P@s\$vv0|2|)"
-//        Fuel.get("/basic-auth/$username/$password").authenticate(username, password).responseString { request, response, either ->
-//            updateUI(response, either)
-//        }
-//
-//        Fuel.upload("/post").source { request, url ->
-//            val sd = Environment.getExternalStorageDirectory();
-//            val location = File(sd.getAbsolutePath() + "/test")
-//            location.mkdir()
-//            File(location, "test.tmp")
-//        }.progress { writtenBytes, totalBytes ->
-//            Log.e(TAG, "upload: ${writtenBytes.toFloat() / totalBytes.toFloat()}")
-//        }.responseString { request, response, either ->
-//            updateUI(response, either)
-//        }
-//
-        Fuel.post("http://jsonplaceholder.typicode.com/posts/").responseString { request, response, either ->
-            Log.v(TAG, request.toString())
+        Manager.sharedInstance.basePath = "http://httpbin.org"
+        Manager.sharedInstance.additionalHeaders = mapOf("Device" to "Android")
+        Manager.sharedInstance.additionalParams = mapOf("key" to "value")
+
+        Fuel.get("/get", mapOf("foo" to "foo", "bar" to "bar")).responseString { request, response, either ->
             updateUI(response, either)
         }
 
-        Fuel.post("https://api.themoviedb.org/3/authentication/token/new", mapOf("a" to 5))
-                .responseString { request, response, either ->
-                    Log.v(TAG, request.toString())
-                    updateUI(response, either)
+        Fuel.post("/post", mapOf("foo" to "foo", "bar" to "bar")).responseString { request, response, either ->
+            updateUI(response, either)
         }
 
+        Fuel.put("/put", mapOf("foo" to "foo", "bar" to "bar")).responseString { request, response, either ->
+            updateUI(response, either)
+        }
+
+        Fuel.delete("/delete", mapOf("foo" to "foo", "bar" to "bar")).responseString { request, response, either ->
+            updateUI(response, either)
+        }
+
+        Fuel.download("/bytes/1048").destination { response, url ->
+            val sd = Environment.getExternalStorageDirectory();
+            val location = File(sd.getAbsolutePath() + "/test")
+            location.mkdir()
+            File(location, "test.tmp")
+        }.progress { readBytes, totalBytes ->
+            Log.e(TAG, "download: ${readBytes.toFloat() / totalBytes.toFloat()}")
+        }.responseString { request, response, either ->
+            updateUI(response, either)
+        }
+
+        val username = "username"
+        val password = "P@s\$vv0|2|)"
+        Fuel.get("/basic-auth/$username/$password").authenticate(username, password).responseString { request, response, either ->
+            updateUI(response, either)
+        }
+
+        Fuel.upload("/post").source { request, url ->
+            val sd = Environment.getExternalStorageDirectory();
+            val location = File(sd.getAbsolutePath() + "/test")
+            location.mkdir()
+            File(location, "test.tmp")
+        }.progress { writtenBytes, totalBytes ->
+            Log.e(TAG, "upload: ${writtenBytes.toFloat() / totalBytes.toFloat()}")
+        }.responseString { request, response, either ->
+            updateUI(response, either)
+        }
     }
 
     fun updateUI(response: Response, either: Either<FuelError, String>) {
