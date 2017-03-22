3f6c5f8af52869fa9cd1a7d8142eec3fb683dac2
Kittinun Vantasin <kittinun.f@gmail.com>
2017-03-22 20:37:08 +0900

    :white_check_mark: add test to show potential cert problem

--- fuel-android/src/test/kotlin/com/github/kittinunf/fuel/android/RequestAndroidSyncTest.kt
+++ fuel-android/src/test/kotlin/com/github/kittinunf/fuel/android/RequestAndroidSyncTest.kt
@@ -9,6 +9,11 @@ import org.json.JSONObject
 import org.junit.Assert.assertThat
 import org.junit.Test
 import java.net.HttpURLConnection
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 import org.hamcrest.CoreMatchers.`is` as isEqualTo
 
 class RequestAndroidSyncTest : BaseTestCase() {
@@ -17,10 +22,41 @@ class RequestAndroidSyncTest : BaseTestCase() {
         FuelManager.instance.basePath = "https://httpbin.org"
         FuelManager.instance.baseHeaders = mapOf("foo" to "bar")
         FuelManager.instance.baseParams = listOf("key" to "value")
+
+        //configure SSLContext that accepts any cert, you should not do this in your app but this is in test ¯\_(ツ)_/¯
+
+        val acceptsAllTrustManager = object : X509TrustManager {
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+
+            override fun getAcceptedIssuers(): Array<X509Certificate>? = null
+
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+        }
+
+        FuelManager.instance.socketFactory = {
+            val context = SSLContext.getInstance("TLS")
+            context.init(null, arrayOf<TrustManager>(acceptsAllTrustManager), SecureRandom())
+            SSLContext.setDefault(context)
+            context.socketFactory
+        }()
     }
 
     @Test
-    fun httpSyncRequestTest() {
+    fun httpSyncRequestStringTest() {
+        val (request, response, result) = "/get".httpGet(listOf("hello" to "world")).responseString()
+        val (data, error) = result
+
+        assertThat(request, notNullValue())
+        assertThat(response, notNullValue())
+        assertThat(error, nullValue())
+        assertThat(data, notNullValue())
+        assertThat(data as String, isA(String::class.java))
+
+        assertThat(response.httpStatusCode, isEqualTo(HttpURLConnection.HTTP_OK))
+    }
+
+    @Test
+    fun httpSyncRequestJsonTest() {
         val (request, response, result) = "/get".httpGet(listOf("hello" to "world")).responseJson()
         val (data, error) = result
 
