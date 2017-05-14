c2bbe2758a9296d322c3632a461c01f1e92b992c
Tipatai Puthanukunkit <judrummercpe@gmail.com>
2017-05-14 19:40:08 +0700

    :boom: Migrate to RxJava2 and kotlin 1.1.2 (#164)

--- fuel-rxjava/src/main/kotlin/com/github/kittinunf/fuel/rx/RxFuel.kt
+++ fuel-rxjava/src/main/kotlin/com/github/kittinunf/fuel/rx/RxFuel.kt
@@ -1,14 +1,10 @@
 package com.github.kittinunf.fuel.rx
 
-import com.github.kittinunf.fuel.core.Deserializable
-import com.github.kittinunf.fuel.core.Request
-import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.*
 import com.github.kittinunf.fuel.core.deserializers.ByteArrayDeserializer
 import com.github.kittinunf.fuel.core.deserializers.StringDeserializer
-import com.github.kittinunf.fuel.core.response
 import com.github.kittinunf.result.Result
-import rx.Observable
-import rx.subjects.AsyncSubject
+import io.reactivex.Single
 import java.nio.charset.Charset
 
 fun Request.rx_response() = rx_response(ByteArrayDeserializer())
@@ -17,22 +13,11 @@ fun Request.rx_responseString(charset: Charset = Charsets.UTF_8) = rx_response(S
 
 fun <T : Any> Request.rx_responseObject(deserializable: Deserializable<T>) = rx_response(deserializable)
 
-private fun <T : Any> Request.rx_response(deserializable: Deserializable<T>): Observable<Pair<Response, T>> =
-        Observable.defer {
-            val source = AsyncSubject.create<Pair<Response, T>>()
-            response(deserializable) { request, response, result ->
-                when (result) {
-                    is Result.Success -> {
-                        source.onNext(response to result.value)
-                        source.onCompleted()
-                    }
-
-                    is Result.Failure -> {
-                        source.onError(result.error)
-                    }
-                }
-            }
-            source.doOnUnsubscribe { this.cancel() }
+private fun <T : Any> Request.rx_response(deserializable: Deserializable<T>): Single<Pair<Response, Result<T, FuelError>>> =
+        Single.create { emitter ->
+            val (_, response, result) = response(deserializable)
+            emitter.onSuccess(response to result)
+            emitter.setCancellable { this.cancel() }
         }
 
 fun Request.rx_bytes() = rx_result(ByteArrayDeserializer())
@@ -41,21 +26,10 @@ fun Request.rx_string(charset: Charset = Charsets.UTF_8) = rx_result(StringDeser
 
 fun <T : Any> Request.rx_object(deserializable: Deserializable<T>) = rx_result(deserializable)
 
-private fun <T : Any> Request.rx_result(deserializable: Deserializable<T>): Observable<T> =
-        Observable.defer {
-            val source = AsyncSubject.create<T>()
-            response(deserializable) { request, response, result ->
-                when (result) {
-                    is Result.Success -> {
-                        source.onNext(result.value)
-                        source.onCompleted()
-                    }
-
-                    is Result.Failure -> {
-                        source.onError(result.error)
-                    }
-                }
-            }
-            source.doOnUnsubscribe { this.cancel() }
+private fun <T : Any> Request.rx_result(deserializable: Deserializable<T>): Single<Result<T, FuelError>> =
+        Single.create { emitter ->
+            val (_, _, result) = response(deserializable)
+            emitter.onSuccess(result)
+            emitter.setCancellable { this.cancel() }
         }
 
