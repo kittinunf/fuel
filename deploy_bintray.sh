50b9c9366a76cd5b21eb976917ae9d5392a7cde2
Kittinun Vantasin <kittinun.f@gmail.com>
2017-07-20 00:30:07 +0900

    :sparkles: Prepare for version 1.9 (#200)

--- deploy_bintray.sh
+++ deploy_bintray.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 if [[ "$TRAVIS_BRANCH" == release-v* ]]; then
+
   echo "We're on release branch, deploying"
-  ./gradlew :fuel:clean :fuel:build :fuel:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
-  ./gradlew :fuel-android:clean :fuel-android:build :fuel-android:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false -x mavenAndroidJavadocs
-  ./gradlew :fuel-rxjava:clean :fuel-rxjava:build :fuel-rxjava:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false -x mavenAndroidJavadocs
-  ./gradlew :fuel-livedata:clean :fuel-livedata:build :fuel-livedata:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false -x mavenAndroidJavadocs
+
+  modules=("fuel" "fuel-android" "fuel-rxjava" "fuel-livedata" "fuel-gson")
+
+  for i in "${modules[@]}"
+  do
+    ./gradlew :$i:clean :$i:build :$i:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
+  done
+
 fi
