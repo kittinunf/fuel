1b099d5429e6740985be945a05ba2b9f77d2a50e
Parker Kuivila <ParkerK@users.noreply.github.com>
2017-08-17 09:57:44 -0400

    Check for appropriate 3xx redirects and replace a try catch with an if/else
    A few notes:
    
    1) I replace the if (3xx || 3xx || 3xx ...) with a when(), just to make it look a little cleaner. I can revert this if you prefer the if()
    2) I noticed the `try`/`catch` used before was simply to check if the url was relative or absolute. I replaced this with URI.isAbsolute to check instead, as there's a little less overhead.

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/interceptors/RedirectionInterceptor.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/interceptors/RedirectionInterceptor.kt
@@ -1,43 +1,54 @@
+
 package com.github.kittinunf.fuel.core.interceptors
 
 import com.github.kittinunf.fuel.core.*
 import java.net.MalformedURLException
+import java.net.URI
 import java.net.URL
-import javax.net.ssl.HttpsURLConnection
+import java.net.HttpURLConnection.HTTP_MULT_CHOICE //300
+import java.net.HttpURLConnection.HTTP_MOVED_PERM //301
+import java.net.HttpURLConnection.HTTP_MOVED_TEMP//302
+import java.net.HttpURLConnection.HTTP_SEE_OTHER//303
 
 class RedirectException : Exception("Redirection fail, not found URL to redirect to")
-val HTTP_3XX_RANGE = 300..399
 
 fun redirectResponseInterceptor(manager: FuelManager) =
         { next: (Request, Response) -> Response ->
             { request: Request, response: Response ->
-                if (HTTP_3XX_RANGE.contains(response.httpStatusCode)) { // Check if response is 3xx HTTP Status Code
-                    val redirectedUrl = response.httpResponseHeaders["Location"]
-                    if (redirectedUrl != null && !redirectedUrl.isEmpty()) {
-                        val encoding = Encoding().apply {
-                            httpMethod = request.httpMethod
-                            urlString =
-                            try {
-                                URL(redirectedUrl[0]).toString()
-                            } catch (e: MalformedURLException){
-                                // Maybe its a relative url. Use the original for context.
-                                URL(request.url, redirectedUrl[0]).toString()
+                when (response.httpStatusCode) {
+                    HTTP_MULT_CHOICE, //300
+                    HTTP_MOVED_PERM, //301
+                    HTTP_MOVED_TEMP, //302
+                    HTTP_SEE_OTHER, //303
+                    307,
+                    308 -> {
+                        val redirectedUrl = response.httpResponseHeaders["Location"]
+                        if (redirectedUrl != null && !redirectedUrl.isEmpty()) {
+                            val encoding = Encoding().apply {
+                                httpMethod = request.httpMethod
+                                urlString =
+                                        if (URI(redirectedUrl[0]).isAbsolute) {
+                                            URL(redirectedUrl[0]).toString()
+                                        } else {
+                                            // Maybe its a relative url. Use the original for context.
+                                            URL(request.url, redirectedUrl[0]).toString()
+                                        }
                             }
+                            manager.request(encoding).response().second
+                        } else {
+                            //error
+                            val error = FuelError().apply {
+                                exception = RedirectException()
+                                errorData = response.data
+                                this.response = response
+                            }
+                            throw error
                         }
-                        manager.request(encoding).response().second
-                    } else {
-                        //error
-                        val error = FuelError().apply {
-                            exception = RedirectException()
-                            errorData = response.data
-                            this.response = response
-                        }
-                        throw error
                     }
-                } else {
-                    next(request, response)
+                    else -> {
+                        next(request, response)
+                    }
                 }
             }
         }
-
 
