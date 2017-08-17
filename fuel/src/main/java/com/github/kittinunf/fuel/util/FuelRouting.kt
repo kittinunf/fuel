83166a72171a4003b92d6edcc111ef769baeacad
Matteo Crippa <matteo@boostco.de>
2017-08-17 20:31:12 +0200

    added fuelrouting v1.0, with logs stripped out

---
+++ fuel/src/main/java/com/github/kittinunf/fuel/util/FuelRouting.kt
@@ -0,0 +1,53 @@
+package retrofitpoc.boostcode.retrofitpoc
+
+import android.util.Log
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.Encoding
+import com.github.kittinunf.fuel.core.Method
+import com.github.kittinunf.fuel.core.Request
+
+/**
+ * Created by matteocrippa on 8/16/17.
+ */
+interface FuelRouting: Fuel.RequestConvertible {
+    // contain the base path of the call
+    val basePath: String
+    // contain the method for the call
+    val method: Method
+    // contain the path for the call
+    val path: String
+    // contain the parameters for the call
+    val params: List<Pair<String, Any?>>?
+    // contain the headers for the call
+    val headers: Map<String, String>?
+
+    // debug enabler
+    val isDebug: Boolean
+
+    // request variable according Fuel.RequestConvertible interface
+    override val request: Request
+        get() {
+            // generate the encoder according provided parameters, headers, path, etc.
+            val encoder = Encoding().apply {
+                this.baseUrlString = basePath
+                this.httpMethod = method
+                this.urlString = path
+                this.parameters = params
+                // FIXME: headers are missing
+            }
+
+            // check if debug mode is on
+            // TODO: consider to strip off on release
+            if (isDebug) {
+                Log.d("⛽ base path", basePath)
+                Log.d("⛽ path", path)
+                Log.d("⛽ method", method.toString())
+                Log.d("⛽ parameters", params.toString())
+                Log.d("⛽ headers", headers.toString())
+                Log.d("⛽ encoder", encoder.request.cUrlString())
+            }
+
+            // return the generated encoder
+            return encoder.request
+        }
+}
