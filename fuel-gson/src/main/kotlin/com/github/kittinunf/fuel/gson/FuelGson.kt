498ab464d5cc0708a1a72a6ec88bb752baa86dd3
Yoav Sternberg <yoav.sternberg@gmail.com>
2017-09-04 20:18:57 +0300

    :ambulance: Fix json typing

--- fuel-gson/src/main/kotlin/com/github/kittinunf/fuel/gson/FuelGson.kt
+++ fuel-gson/src/main/kotlin/com/github/kittinunf/fuel/gson/FuelGson.kt
@@ -5,21 +5,25 @@ import com.github.kittinunf.result.Result
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.Reader
+import java.lang.reflect.Type
 
 /**
  * Created by ihor_kucherenko on 7/4/17.
  * https://github.com/KucherenkoIhor
+ *
+ * Note: Use the default constructor iff no generics
  */
-class GsonDeserializer<out T : Any> : ResponseDeserializable<T> {
+class GsonDeserializer<out T : Any>(private val type: Type = (object : TypeToken<T>(){} as TypeToken<*>).type) : ResponseDeserializable<T> {
 
-    override fun deserialize(reader: Reader): T
-            = Gson().fromJson<T>(reader, object : TypeToken<T>() {}.type)
+    override fun deserialize(reader: Reader): T = Gson().fromJson<T>(reader, type)
 
 }
 
-fun <T : Any> Request.responseObject(handler: (Request, Response, Result<T, FuelError>) -> Unit) = response(GsonDeserializer<T>(), handler)
+inline fun <reified T : Any> Request.responseObject(noinline handler: (Request, Response, Result<T, FuelError>) -> Unit) =
+        response(GsonDeserializer<T>(object : TypeToken<T>(){}.type), handler)
 
-fun <T : Any> Request.responseObject(handler: Handler<T>) = response(GsonDeserializer<T>(), handler)
+inline fun <reified T : Any> Request.responseObject(handler: Handler<T>) = response(GsonDeserializer<T>(object : TypeToken<T>(){}.type), handler)
 
-fun <T : Any> Request.responseObject() = response(GsonDeserializer<T>())
+inline fun <reified T : Any> Request.responseObject() = response(GsonDeserializer<T>(object : TypeToken<T>(){}.type))
+
 
