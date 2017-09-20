a7e06d3ff1c6f0ac265d3d83624c338572dc8990
Timo Sand <timo.sand@iki.fi>
2017-09-20 18:58:15 +0300

    :sparkles: Add Jackson deserialization support (#237)

---
+++ fuel-jackson/src/main/kotlin/com/github/kittiunf/fuel/jackson/FuelJackson.kt
@@ -0,0 +1,39 @@
+package com.github.kittiunf.fuel.jackson
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.github.kittinunf.fuel.core.*
+import com.github.kittinunf.result.Result
+import java.io.InputStream
+import java.io.Reader
+
+val mapper : ObjectMapper = ObjectMapper().registerKotlinModule().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+inline fun <reified T : Any> Request.responseObject(noinline handler: (Request, Response, Result<T, FuelError>) -> Unit) {
+    response(jacksonDeserializerOf(), handler)
+}
+
+inline fun <reified T : Any> Request.responseObject(handler: Handler<T>) = response(jacksonDeserializerOf(), handler)
+
+inline fun <reified T : Any> Request.responseObject() = response(jacksonDeserializerOf())
+
+
+inline fun <reified T : Any> jacksonDeserializerOf() = object : ResponseDeserializable<T> {
+    override fun deserialize(reader: Reader): T? {
+        return mapper.readValue(reader)
+    }
+
+    override fun deserialize(content: String): T? {
+        return mapper.readValue(content)
+    }
+
+    override fun deserialize(bytes: ByteArray): T? {
+        return mapper.readValue(bytes)
+    }
+
+    override fun deserialize(inputStream: InputStream): T? {
+        return mapper.readValue(inputStream)
+    }
+}
---
+++ fuel-jackson/src/test/kotlin/com/github/kittinunf/fuel/FuelJacksonTest.kt
@@ -0,0 +1,134 @@
+package com.github.kittinunf.fuel
+
+import com.github.kittinunf.fuel.core.*
+import com.github.kittinunf.result.Result
+import com.github.kittiunf.fuel.jackson.jacksonDeserializerOf
+import com.github.kittiunf.fuel.jackson.responseObject
+import org.hamcrest.CoreMatchers.*
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertThat
+import org.junit.Test
+
+class FuelJacksonTest {
+
+    init {
+        FuelManager.instance.basePath = "https://httpbin.org"
+
+        Fuel.testMode {
+            timeout = 15000
+        }
+    }
+
+    //Model
+    data class HttpBinUserAgentModel(var userAgent: String = "")
+
+    @Test
+    fun jacksonTestResponseObject() {
+        Fuel.get("/user-agent")
+                .responseObject(jacksonDeserializerOf<HttpBinUserAgentModel>()) { _, _, result ->
+                    assertThat(result.component1(), instanceOf(HttpBinUserAgentModel::class.java))
+                    assertThat(result.component1()?.userAgent, not(""))
+                    assertThat(result.component2(), instanceOf(FuelError::class.java))
+                }
+    }
+
+    @Test
+    fun jacksonTestResponseObjectError() {
+        Fuel.get("/useragent")
+                .responseObject(jacksonDeserializerOf<HttpBinUserAgentModel>()) { _, _, result ->
+                    assertThat(result.component1(), notNullValue())
+                    assertThat(result.component2(), instanceOf(Result.Failure::class.java))
+                }
+    }
+
+    @Test
+    fun jacksonTestResponseDeserializerObject() {
+        Fuel.get("/user-agent")
+                .responseObject<HttpBinUserAgentModel> { _, _, result ->
+                    assertThat(result.component1(), notNullValue())
+                    assertThat(result.component2(), notNullValue())
+                }
+    }
+
+    @Test
+    fun jacksonTestResponseDeserializerObjectError() {
+        Fuel.get("/useragent")
+                .responseObject<HttpBinUserAgentModel> { _, _, result ->
+                    assertThat(result.component1(), notNullValue())
+                    assertThat(result.component2(), instanceOf(Result.Failure::class.java))
+                }
+    }
+
+    @Test
+    fun jacksonTestResponseHandlerObject() {
+        Fuel.get("/user-agent")
+                .responseObject(object : Handler<HttpBinUserAgentModel> {
+                    override fun success(request: Request, response: Response, value: HttpBinUserAgentModel) {
+                        assertThat(value, notNullValue())
+                    }
+
+                    override fun failure(request: Request, response: Response, error: FuelError) {
+                        assertThat(error, notNullValue())
+                    }
+
+                })
+    }
+
+    @Test
+    fun jacksonTestResponseHandlerObjectError() {
+        Fuel.get("/useragent")
+                .responseObject(object : Handler<HttpBinUserAgentModel> {
+                    override fun success(request: Request, response: Response, value: HttpBinUserAgentModel) {
+                        assertThat(value, notNullValue())
+                    }
+
+                    override fun failure(request: Request, response: Response, error: FuelError) {
+                        assertThat(error, instanceOf(Result.Failure::class.java))
+                    }
+
+                })
+    }
+
+    @Test
+    fun jacksonTestResponseSyncObject() {
+        val triple = Fuel.get("/user-agent").responseObject<HttpBinUserAgentModel>()
+        assertThat(triple.third.component1(), notNullValue())
+    }
+
+    @Test
+    fun jacksonTestResponseSyncObjectError() {
+        val triple = Fuel.get("/useragent").responseObject<HttpBinUserAgentModel>()
+        assertThat(triple.third.component2(), instanceOf(FuelError::class.java))
+    }
+
+    data class IssueInfo(val id: Int, val title: String, val number: Int)
+
+    @Test
+    fun testProcessingGenericList() {
+        Fuel.get("https://api.github.com/repos/kittinunf/Fuel/issues").responseObject<List<IssueInfo>> { _, _, result ->
+            val issues = result.get()
+            assertNotEquals(issues.size, 0)
+            assertThat(issues[0], isA(IssueInfo::class.java))
+        }
+    }
+
+    @Test
+    fun manualDeserializationShouldWork() {
+        Fuel.get("https://api.github.com/repos/kittinunf/Fuel/issues").response { _: Request, response: Response, result: Result<ByteArray, FuelError> ->
+            var issueList = jacksonDeserializerOf<List<IssueInfo>>().deserialize(response)
+            assertThat(issueList[0], isA(IssueInfo::class.java))
+            issueList = jacksonDeserializerOf<List<IssueInfo>>().deserialize(response.dataStream)!!
+            assertThat(issueList[0], isA(IssueInfo::class.java))
+            issueList = jacksonDeserializerOf<List<IssueInfo>>().deserialize(response.dataStream.reader())!!
+            assertThat(issueList[0], isA(IssueInfo::class.java))
+            issueList = jacksonDeserializerOf<List<IssueInfo>>().deserialize(result.get())!!
+            assertThat(issueList[0], isA(IssueInfo::class.java))
+        }
+        Fuel.get("https://api.github.com/repos/kittinunf/Fuel/issues").responseString { _: Request, _: Response, result: Result<String, FuelError> ->
+            val issueList = jacksonDeserializerOf<List<IssueInfo>>().deserialize(result.get())!!
+            assertThat(issueList[0], isA(IssueInfo::class.java))
+
+        }
+
+    }
+}
