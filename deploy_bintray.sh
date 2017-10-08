c7f5b7f8061b1122a02d722c9d147a9fd09351cf
Kittinun Vantasin <kittinun.f@gmail.com>
2017-10-08 16:36:33 +0900

    Escape error with Android related modules

--- deploy_bintray.sh
+++ deploy_bintray.sh
@@ -4,20 +4,12 @@ if [[ "$TRAVIS_BRANCH" == */release-v* ]]; then
 
   echo "We're on release branch, deploying at $TRAVIS_BRANCH"
 
-  modules=("fuel" "fuel-rxjava" "fuel-jackson" "fuel-gson" "fuel-livedata")
+  modules=("fuel" "fuel-android" "fuel-rxjava" "fuel-jackson" "fuel-gson" "fuel-livedata")
   for i in "${modules[@]}"
   do
     echo ">> Deploying $i ..."
     ./gradlew :$i:clean :$i:build :$i:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
     echo ">> Done deploying for $i"
-  done
-
-  android_modules=("fuel-android")
-  for j in "${android_modules[@]}"
-  do
-    echo ">> Deploying $j ..."
-    ./gradlew :$j:clean :$j:build :$j:bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false -x mavenAndroidJavadocs
-    echo ">> Done deploying for $j"
   done
 
 fi
