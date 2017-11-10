f668701865689a5e87cf6fe7fb84fefb1a9c2f0b
Christopher Ng <babedroid@gmail.com>
2017-11-10 22:25:34 +0700

    :sparkles: Support Forge

---
+++ fuel-forge/src/main/kotlin/com/github/babedev/fuel/forge/FuelForge.kt
@@ -0,0 +1,41 @@
+package com.github.babedev.fuel.forge
+
+import com.github.kittinunf.forge.Forge
+import com.github.kittinunf.forge.core.DeserializedResult
+import com.github.kittinunf.forge.core.JSON
+import com.github.kittinunf.fuel.core.*
+import com.github.kittinunf.result.Result
+
+inline fun <reified T : Any> Request.responseObject(noinline deserializer: JSON.() -> DeserializedResult<T>, noinline handler: (Request, Response, Result<T, FuelError>) -> Unit) =
+        response(forgeDeserializerOf(deserializer), handler)
+
+inline fun <reified T : Any> Request.responseObjects(noinline deserializer: JSON.() -> DeserializedResult<T>, noinline handler: (Request, Response, Result<List<T>, FuelError>) -> Unit) =
+        response(forgesDeserializerOf(deserializer), handler)
+
+inline fun <reified T : Any> Request.responseObject(noinline deserializer: JSON.() -> DeserializedResult<T>, handler: Handler<T>) = response(forgeDeserializerOf(deserializer), handler)
+
+inline fun <reified T : Any> forgeDeserializerOf(noinline deserializer: JSON.() -> DeserializedResult<T>) = object : ResponseDeserializable<T> {
+
+    override fun deserialize(content: String): T? {
+        val result = Forge.modelFromJson(content, deserializer)
+        var res: T? = null
+
+        result.fold({
+            res = it as T
+        }, {
+            res = null
+        })
+
+        return res
+    }
+
+}
+
+inline fun <reified T : Any> forgesDeserializerOf(noinline deserializer: JSON.() -> DeserializedResult<T>) = object : ResponseDeserializable<List<T>> {
+
+    override fun deserialize(content: String): List<T>? {
+        val result = Forge.modelsFromJson(content, deserializer)
+        return result.map { it.get<T>() }
+    }
+
+}
