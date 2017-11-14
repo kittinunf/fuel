b5d7443284884a82f9166d75d510e1ac054ab1e3
Woramet Muangsiri <krw127@hotmail.com>
2017-11-14 15:50:09 +0900

    Remove setContentView

--- sample-java/src/main/java/com/example/fuel/MainActivity.java
+++ sample-java/src/main/java/com/example/fuel/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         binding.mainGoButton.setOnClickListener(new View.OnClickListener() {
