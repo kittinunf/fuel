dd13a361954e7685bf24471d1807823212fba999
Kittinun Vantasin <kittinun.f@gmail.com>
2017-11-14 23:31:07 +0900

    :bug: Fix type issue with usage with Jackson in sync mode (#268)
    * This represents type issue when use with jackson sync mode
    
    * Fix type issue
    
    * Use github api
    
    * Fix tests

--- fuel-jackson/src/main/kotlin/com/github/kittinunf/fuel/jackson/FuelJackson.kt
+++ fuel-jackson/src/main/kotlin/com/github/kittinunf/fuel/jackson/FuelJackson.kt
@@ -1,15 +1,21 @@
-package com.github.kittiunf.fuel.jackson
+package com.github.kittinunf.fuel.jackson
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import com.github.kittinunf.fuel.core.*
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.Handler
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.github.kittinunf.fuel.core.response
 import com.github.kittinunf.result.Result
 import java.io.InputStream
 import java.io.Reader
 
-val mapper : ObjectMapper = ObjectMapper().registerKotlinModule().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+val mapper = ObjectMapper().registerKotlinModule()
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
 inline fun <reified T : Any> Request.responseObject(noinline handler: (Request, Response, Result<T, FuelError>) -> Unit) {
     response(jacksonDeserializerOf(), handler)
@@ -17,8 +23,7 @@ inline fun <reified T : Any> Request.responseObject(noinline handler: (Request, 
 
 inline fun <reified T : Any> Request.responseObject(handler: Handler<T>) = response(jacksonDeserializerOf(), handler)
 
-inline fun <reified T : Any> Request.responseObject() = response(jacksonDeserializerOf())
-
+inline fun <reified T : Any> Request.responseObject() = response(jacksonDeserializerOf<T>())
 
 inline fun <reified T : Any> jacksonDeserializerOf() = object : ResponseDeserializable<T> {
     override fun deserialize(reader: Reader): T? {
