c9a6a990fb11fcc2c6132396a880d4ac2c2b1c87
Matteo Crippa <matteo@boostco.de>
2018-04-03 14:38:45 +0200

    added tests

--- fuel/src/test/kotlin/com/github/kittinunf/fuel/RoutingTest.kt
+++ fuel/src/test/kotlin/com/github/kittinunf/fuel/RoutingTest.kt
@@ -7,6 +7,7 @@ import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.Response
 import com.github.kittinunf.fuel.util.FuelRouting
 import org.hamcrest.CoreMatchers.*
+import org.json.JSONObject
 import org.junit.Assert.assertThat
 import org.junit.Test
 import java.net.HttpURLConnection
@@ -21,36 +22,54 @@ class RoutingTest: BaseTestCase() {
 
         override val basePath = "https://httpbin.org/"
 
-        class getTest: TestApi()
-        class getParamsTest(val name: String, val value: String): TestApi()
+        class GetTest : TestApi()
+        class GetParamsTest(val name: String, val value: String) : TestApi()
+        class GetBodyTest(val value: String) : TestApi()
 
         override val method: Method
             get() {
-                when(this) {
-                    is getTest -> return Method.GET
-                    is getParamsTest -> return Method.GET
+                return when (this) {
+                    is GetTest -> Method.GET
+                    is GetParamsTest -> Method.GET
+                    is GetBodyTest -> Method.POST
                 }
             }
 
         override val path: String
             get() {
-                return when(this) {
-                    is getTest -> "/get"
-                    is getParamsTest -> "/get"
+                return when (this) {
+                    is GetTest -> "/get"
+                    is GetParamsTest -> "/get"
+                    is GetBodyTest -> "/post"
                 }
             }
 
         override val params: List<Pair<String, Any?>>?
             get() {
-                return when(this) {
-                    is getParamsTest -> listOf(this.name to this.value)
+                return when (this) {
+                    is GetParamsTest -> listOf(this.name to this.value)
+                    else -> null
+                }
+            }
+
+        override val body: String?
+            get() {
+                return when (this) {
+                    is GetParamsTest -> {
+                        val json = JSONObject()
+                        json.put("id", this.value)
+                        json.toString()
+                    }
                     else -> null
                 }
             }
 
         override val headers: Map<String, String>?
             get() {
-                return null
+                return when (this) {
+                    is GetParamsTest -> mapOf("Content-Type" to "application/json")
+                    else -> null
+                }
             }
 
     }
@@ -62,7 +81,7 @@ class RoutingTest: BaseTestCase() {
         var data: Any? = null
         var error: FuelError? = null
 
-        manager.request(TestApi.getTest()).responseString { req, res, result ->
+        manager.request(TestApi.GetTest()).responseString { req, res, result ->
             request = req
             response = res
 
@@ -91,7 +110,7 @@ class RoutingTest: BaseTestCase() {
         val paramKey = "foo"
         val paramValue = "bar"
 
-        manager.request(TestApi.getParamsTest(name = paramKey, value = paramValue)).responseString { req, res, result ->
+        manager.request(TestApi.GetParamsTest(name = paramKey, value = paramValue)).responseString { req, res, result ->
             request = req
             response = res
 
@@ -112,5 +131,36 @@ class RoutingTest: BaseTestCase() {
 
         assertThat(string, containsString(paramKey))
         assertThat(string, containsString(paramValue))
+    }
+
+    @Test
+    fun httpRouterGetBody() {
+        var request: Request? = null
+        var response: Response? = null
+        var data: Any? = null
+        var error: FuelError? = null
+
+        val paramValue = "42"
+
+        manager.request(TestApi.GetBodyTest(paramValue)).responseString { req, res, result ->
+            request = req
+            response = res
+
+            val (d, err) = result
+            data = d
+            error = err
+        }
+
+        val string = data as String
+
+        assertThat(request, notNullValue())
+        assertThat(response, notNullValue())
+        assertThat(error, nullValue())
+        assertThat(data, notNullValue())
+
+        val statusCode = HttpURLConnection.HTTP_OK
+        assertThat(response?.statusCode, isEqualTo(statusCode))
+
+        assertThat(string, containsString("42"))
     }
 }
