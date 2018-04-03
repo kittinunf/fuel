4cb0a0603e4e32778d148eb4900ee836d7b7a77b
Matteo Crippa <matteo@boostco.de>
2018-04-03 15:14:33 +0200

    strip out check, need to rework

--- fuel/src/test/kotlin/com/github/kittinunf/fuel/RequestTest.kt
+++ fuel/src/test/kotlin/com/github/kittinunf/fuel/RequestTest.kt
@@ -1,6 +1,11 @@
 package com.github.kittinunf.fuel
 
-import com.github.kittinunf.fuel.core.*
+import com.github.kittinunf.fuel.core.Encoding
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.Method
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
 import org.hamcrest.CoreMatchers.*
 import org.junit.Assert.assertThat
 import org.junit.Test
