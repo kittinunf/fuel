aa9dfd0377419392230e1e9119bd670a9a54104e
MarkGilchrist <theheadchef@gameforeverything.com>
2018-07-13 19:27:22 +0100

    updated implementation to higher absraction (iterable)

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/Fuel.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/Fuel.kt
@@ -129,7 +129,7 @@ class Fuel {
 
 @JvmOverloads
 fun String.httpGet(parameters: List<Pair<String, Any?>>? = null): Request = Fuel.get(this, parameters?.flatMap { pair ->
-    (pair.second as? Array<*>)?.let {
+    (pair.second as? Iterable<*>)?.let {
         it.map { "${pair.first}[]" to it }.toList()
     } ?: listOf(pair)
 })
