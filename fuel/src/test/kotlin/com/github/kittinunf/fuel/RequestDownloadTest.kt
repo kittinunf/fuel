71387336026bbd70d641b6af4279b21dcb6f1e00
MarkGilchrist <theheadchef@gameforeverything.com>
2018-07-31 07:37:36 +0100

    added more tests

--- fuel/src/test/kotlin/com/github/kittinunf/fuel/RequestDownloadTest.kt
+++ fuel/src/test/kotlin/com/github/kittinunf/fuel/RequestDownloadTest.kt
@@ -5,6 +5,7 @@ import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.core.Request
 import com.github.kittinunf.fuel.core.Response
 import org.hamcrest.CoreMatchers.*
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThat
 import org.junit.Test
 import java.io.File
@@ -51,6 +52,47 @@ class RequestDownloadTest : BaseTestCase() {
     }
 
     @Test
+    fun httpDownloadWithProgressValidCaseResponse() {
+        var request: Request? = null
+        var response: Response? = null
+        var data: Any? = null
+        var error: FuelError? = null
+
+        var read = -1L
+        var total = -1L
+
+        val numberOfBytes = 19976L
+        val file = File.createTempFile(numberOfBytes.toString(), null)
+
+        manager.download("/bytes/$numberOfBytes").destination { _, _ ->
+            println(file.absolutePath)
+            file
+        }.progress { readBytes, totalBytes ->
+            read = readBytes
+            total = totalBytes
+            println("read: $read, total: $total")
+        }.response { req, res, result ->
+            request = req
+            response = res
+            val (d, err) = result
+            data = d
+            error = err
+        }
+
+        assertThat(request, notNullValue())
+        assertThat(response, notNullValue())
+        assertThat(error, nullValue())
+        assertThat(data, notNullValue())
+        assertEquals(data is ByteArray,true)
+        assertEquals((data as ByteArray).size.toLong(),read)
+        assertThat(file.length(),isEqualTo(numberOfBytes))
+
+        assertThat("read bytes and total bytes should be equal", read == total && read != -1L && total != -1L, isEqualTo(true))
+        val statusCode = HttpURLConnection.HTTP_OK
+        assertThat(response?.statusCode, isEqualTo(statusCode))
+    }
+
+    @Test
     fun httpDownloadWithProgressValidCase() {
         var request: Request? = null
         var response: Response? = null
@@ -60,7 +102,7 @@ class RequestDownloadTest : BaseTestCase() {
         var read = -1L
         var total = -1L
 
-        val numberOfBytes = 1048576L
+        val numberOfBytes = 41924L
         val file = File.createTempFile(numberOfBytes.toString(), null)
 
         manager.download("/bytes/$numberOfBytes").destination { _, _ ->
@@ -82,6 +124,7 @@ class RequestDownloadTest : BaseTestCase() {
         assertThat(response, notNullValue())
         assertThat(error, nullValue())
         assertThat(data, notNullValue())
+        assertThat(file.length(),isEqualTo(response?.data?.size?.toLong() ?: 0L))
         assertThat(file.length(),isEqualTo(numberOfBytes))
 
         assertThat("read bytes and total bytes should be equal", read == total && read != -1L && total != -1L, isEqualTo(true))
@@ -116,7 +159,7 @@ class RequestDownloadTest : BaseTestCase() {
         assertThat(response, notNullValue())
         assertThat(error, notNullValue())
         assertThat(data, nullValue())
-        assertThat(file.length(),isEqualTo(numberOfBytes))
+        assertThat(file.length(),isEqualTo(0L))
 
         val statusCode = HttpURLConnection.HTTP_NOT_FOUND
         assertThat(response?.statusCode, isEqualTo(statusCode))
@@ -129,7 +172,7 @@ class RequestDownloadTest : BaseTestCase() {
         var data: Any? = null
         var error: FuelError? = null
 
-        val numberOfBytes = 131072
+        val numberOfBytes = 131072L
         manager.download("/bytes/$numberOfBytes").destination { _, _ ->
             val dir = System.getProperty("user.dir")
             File.createTempFile("not_found_file", null, File(dir, "not-a-folder"))
@@ -188,7 +231,7 @@ class RequestDownloadTest : BaseTestCase() {
         assertThat(response, notNullValue())
         assertThat(error, nullValue())
         assertThat(data, notNullValue())
-        assertThat(file.length(),isEqualTo(104857600L))
+        assertThat(file.length(),isEqualTo(100L * 1024L * 1024L))
 
         assertThat("read bytes and total bytes should be equal", read == total && read != -1L && total != -1L, isEqualTo(true))
         val statusCode = HttpURLConnection.HTTP_OK
