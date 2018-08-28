5c2e9b1257ba434e203e4e2347ae31098f4c0777
MarkGilchrist <theheadchef@gameforeverything.com>
2018-08-28 18:54:48 +0100

    removed print ln

--- fuel/src/test/kotlin/com/github/kittinunf/fuel/RequestTest.kt
+++ fuel/src/test/kotlin/com/github/kittinunf/fuel/RequestTest.kt
@@ -361,7 +361,7 @@ class RequestTest : BaseTestCase() {
         }
 
         val string = data as String
-        println(string)
+       
         assertThat(request, notNullValue())
         assertThat(response, notNullValue())
         assertThat(error, nullValue())
