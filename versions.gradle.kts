9212a4dc32ae49b4bd2295a2fb127fc3faa69f14
Lucas <lucasvalenteds@gmail.com>
2018-09-06 08:26:30 -0300

    Organize versions.gradle.kts file
    * Rename comments pattern
    * Move versions to the expected category

--- versions.gradle.kts
+++ versions.gradle.kts
@@ -1,25 +1,28 @@
 buildscript {
+    // Library version
     extra["publishVersion"] = "1.15.0"
 
-    //dependencies version
+    // Core dependencies
     extra["kotlinVersion"] = "1.2.51"
     extra["resultVersion"] = "1.5.0"
-    extra["rxjavaVersion"] = "2.1.13"
+
+    // Modules dependencies
     extra["archLifecycleVersion"] = "1.1.1"
-
-    //android dependencies version
-    extra["fuelCompileSdkVersion"] =  27
-    extra["androidSupportVersion"] =  "27.1.1"
-
-    //test dependencies version
-    extra["junitVersion"] = "4.12"
-    extra["robolectricVersion"] = "3.8"
+    extra["forgeVersion"] = "0.3.0"
     extra["gsonVersion"] = "2.8.2"
     extra["jacksonVersion"] = "2.9.6"
-    extra["moshiVersion"] = "1.5.0"
-    extra["runnerVersion"] = "1.0.0"
-    extra["rulesVersion"] = "1.0.0"
-    extra["espressoVersion"] = "3.0.0"
     extra["kotlinCoroutinesVersion"] = "0.23.3"
-    extra["forgeVersion"] = "0.3.0"
+    extra["moshiVersion"] = "1.5.0"
+    extra["rxjavaVersion"] = "2.1.13"
+
+    // Android dependencies
+    extra["androidSupportVersion"] =  "27.1.1"
+    extra["fuelCompileSdkVersion"] =  27
+
+    // Testing dependencies
+    extra["espressoVersion"] = "3.0.0"
+    extra["junitVersion"] = "4.12"
+    extra["robolectricVersion"] = "3.8"
+    extra["rulesVersion"] = "1.0.0"
+    extra["runnerVersion"] = "1.0.0"
 }
