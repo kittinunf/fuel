cfedfdfdb435a81bf1288e3c801c02933df6b124
Jonathan Steele <xfsunoles@gmail.com>
2018-09-22 02:28:05 -0400

    Fixed and Improved Coverage of Moshi.

--- fuel-moshi/src/main/kotlin/com/github/kittinunf/fuel/moshi/FuelMoshi.kt
+++ fuel-moshi/src/main/kotlin/com/github/kittinunf/fuel/moshi/FuelMoshi.kt
@@ -1,22 +1,25 @@
 package com.github.kittinunf.fuel.moshi
 
-import com.github.kittinunf.fuel.core.*
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.core.Handler
+import com.github.kittinunf.fuel.core.Request
+import com.github.kittinunf.fuel.core.Response
+import com.github.kittinunf.fuel.core.ResponseDeserializable
+import com.github.kittinunf.fuel.core.response
 import com.github.kittinunf.result.Result
-import com.squareup.moshi.KotlinJsonAdapterFactory
 import com.squareup.moshi.Moshi
+import kotlin.reflect.KClass
 
 inline fun <reified T : Any> Request.responseObject(noinline handler: (Request, Response, Result<T, FuelError>) -> Unit) =
-        response(moshiDeserializerOf(), handler)
+        response(moshiDeserializerOf(T::class), handler)
 
-inline fun <reified T : Any> Request.responseObject(handler: Handler<T>) = response(moshiDeserializerOf(), handler)
+inline fun <reified T : Any> Request.responseObject(handler: Handler<T>) = response(moshiDeserializerOf(T::class), handler)
 
-inline fun <reified T : Any> Request.responseObject() = response(moshiDeserializerOf<T>())
+inline fun <reified T : Any> Request.responseObject() = response(moshiDeserializerOf(T::class))
 
-inline fun <reified T : Any> moshiDeserializerOf() = object : ResponseDeserializable<T> {
-    override fun deserialize(content: String): T? =
-            Moshi.Builder()
-                    .add(KotlinJsonAdapterFactory())
-                    .build()
-                    .adapter(T::class.java)
-                    .fromJson(content)
+fun <T : Any> moshiDeserializerOf(kClass: KClass<T>) = object : ResponseDeserializable<T> {
+    override fun deserialize(content: String): T? = Moshi.Builder()
+                .build()
+                .adapter(kClass.java)
+                .fromJson(content)
 }
