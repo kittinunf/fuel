ae68b210da24704adccc5def991f6abd31898bf0
MarkGilchrist <theheadchef@gameforeverything.com>
2018-09-24 17:48:38 +0100

    tidied up imports

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Client.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Client.kt
@@ -1,9 +1,6 @@
 package com.github.kittinunf.fuel.core
 
-import com.github.kittinunf.result.Result
-
 interface Client {
     fun executeRequest(request: Request): Response
     suspend fun awaitRequest(request: Request): Response
-    // suspend fun awaitResult(): Result<Response, FuelError>
 }
