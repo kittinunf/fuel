3cd77dd3c1b3076dfd6f3ce84e5ed81f59a04ace
Jonathan <iNoles@users.noreply.github.com>
2018-09-24 11:00:10 -0400

    Fixed and Improved Coverage of Moshi (#439)
    * Fixed and Improved Coverage of Moshi.
    
    * Update FuelMoshi.kt
    
    Change KClass to Java's Class.
    
    * Fixed Two lines of KClass to Class.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,32 +1,32 @@
 object Versions {
     // Library version
-    val publishVersion = "1.15.0"
+    const val publishVersion = "1.15.0"
 
     // Core dependencies
-    val kotlinVersion = "1.2.51"
-    val resultVersion = "1.5.0"
-    val jsonVersion = "20170516"
+    const val kotlinVersion = "1.2.51"
+    const val resultVersion = "1.5.0"
+    const val jsonVersion = "20170516"
 
     // Modules dependencies
-    val androidArchVersion = "1.1.1"
-    val forgeVersion = "0.3.0"
-    val gsonVersion = "2.8.2"
-    val jacksonVersion = "2.9.6"
-    val kotlinCoroutinesVersion = "0.23.3"
-    val moshiVersion = "1.5.0"
-    val rxjavaVersion = "2.1.13"
-    val rxandroidVersion = "2.1.0"
+    const val androidArchVersion = "1.1.1"
+    const val forgeVersion = "0.3.0"
+    const val gsonVersion = "2.8.2"
+    const val jacksonVersion = "2.9.6"
+    const val kotlinCoroutinesVersion = "0.23.3"
+    const val moshiVersion = "1.6.0"
+    const val rxjavaVersion = "2.1.13"
+    const val rxandroidVersion = "2.1.0"
 
     // Android dependencies
-    val androidSupportVersion = "27.1.1"
-    val fuelCompileSdkVersion = 27
-    val fuelMinSdkVersion = 14
+    const val androidSupportVersion = "27.1.1"
+    const val fuelCompileSdkVersion = 27
+    const val fuelMinSdkVersion = 14
 
     // Testing dependencies
-    val espressoVersion = "3.0.0"
-    val junitVersion = "4.12"
-    val robolectricVersion = "3.8"
-    val rulesVersion = "1.0.0"
-    val runnerVersion = "1.0.0"
-    val mockServerVersion = "5.4.1"
+    const val espressoVersion = "3.0.0"
+    const val junitVersion = "4.12"
+    const val robolectricVersion = "3.8"
+    const val rulesVersion = "1.0.0"
+    const val runnerVersion = "1.0.0"
+    const val mockServerVersion = "5.4.1"
 }
