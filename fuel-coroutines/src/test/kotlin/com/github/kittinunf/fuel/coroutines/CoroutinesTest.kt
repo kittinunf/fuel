7d0262e4a2a4fd72e81126c32e93f97008d40141
Jonathan Steele <xfsunoles@gmail.com>
2018-10-04 23:11:39 -0400

    Weird, Java Charset but not Kotlin one.

--- fuel-coroutines/src/test/kotlin/com/github/kittinunf/fuel/coroutines/CoroutinesTest.kt
+++ fuel-coroutines/src/test/kotlin/com/github/kittinunf/fuel/coroutines/CoroutinesTest.kt
@@ -10,6 +10,7 @@ import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import java.nio.charset.Charset
 import java.net.HttpURLConnection
 import java.util.UUID
 import java.util.concurrent.Executors
@@ -198,7 +199,7 @@ class CoroutinesTest {
             response = mock.response().withStatusCode(HttpURLConnection.HTTP_OK).withBody(UUID.randomUUID().toString())
         )
 
-        Fuel.get(mock.path("uuid")).awaitStringResult(threadPoolDispatcher).fold({ data ->
+        Fuel.get(mock.path("uuid")).awaitStringResult(Charset.US-ASCII, threadPoolDispatcher).fold({ data ->
             assertTrue(data.isNotEmpty())
             assertTrue(data + ":" + uuidRegex.toRegex().toString(), uuidRegex.matcher(data).find())
         }, { error ->
