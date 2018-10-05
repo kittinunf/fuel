c91261264a0922c5a2e68e9d9f937a844c22c22d
Jonathan Steele <xfsunoles@gmail.com>
2018-10-05 14:27:06 -0400

    Improve Gson Coverages

--- fuel-gson/src/main/kotlin/com/github/kittinunf/fuel/gson/FuelGson.kt
+++ fuel-gson/src/main/kotlin/com/github/kittinunf/fuel/gson/FuelGson.kt
@@ -8,19 +8,18 @@ import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.github.kittinunf.fuel.core.response
 import com.github.kittinunf.result.Result
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import java.io.Reader
 
 // Created by ihor_kucherenko on 7/4/17.
 //https://github.com/KucherenkoIhor
 
 inline fun <reified T : Any> Request.responseObject(noinline handler: (Request, Response, Result<T, FuelError>) -> Unit) =
-        response(gsonDeserializerOf(), handler)
+        response(gsonDeserializerOf(T::class.java), handler)
 
-inline fun <reified T : Any> Request.responseObject(handler: Handler<T>) = response(gsonDeserializerOf(), handler)
+inline fun <reified T : Any> Request.responseObject(handler: Handler<T>) = response(gsonDeserializerOf(T::class.java), handler)
 
-inline fun <reified T : Any> Request.responseObject() = response(gsonDeserializerOf<T>())
+inline fun <reified T : Any> Request.responseObject() = response(gsonDeserializerOf(T::class.java))
 
-inline fun <reified T : Any> gsonDeserializerOf() = object : ResponseDeserializable<T> {
-    override fun deserialize(reader: Reader): T = Gson().fromJson<T>(reader, object : TypeToken<T>() {}.type)
+fun <T : Any> gsonDeserializerOf(clazz: Class<T>) = object : ResponseDeserializable<T> {
+    override fun deserialize(reader: Reader): T? = Gson().fromJson<T>(reader, clazz)
 }
