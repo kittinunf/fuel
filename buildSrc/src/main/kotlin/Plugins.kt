9e3d0516a6433ae3a667ccf2a90a07135001be68
MarkGilchrist <theheadchef@gameforeverything.com>
2018-10-10 17:22:39 +0100

    updated still no building

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -1,5 +1,5 @@
 object Plugins {
-    const val android = "com.android.tools.build:gradle:3.2.0-alpha10"
+    const val android = "com.android.tools.build:gradle:3.1.3"
     const val jacocoAndroid = "com.dicedmelon.gradle:jacoco-android:0.1.3"
     const val bintray = "com.novoda:bintray-release:0.8.0"
     const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
