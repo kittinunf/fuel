136b41d68f38e807c324e3ea17442d706794b732
Kittinun Vantasin <kittinun.f@gmail.com>
2018-10-20 16:32:45 +0900

    Apply ktlinter

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ plugins {
     id(Jacoco.Android.plugin) version Jacoco.Android.version apply false
     id(BintrayRelease.plugin) version BintrayRelease.version apply false
     id(KotlinX.Serialization.plugin) version Kotlin.version apply false
+    id("org.jmailen.kotlinter") version "1.20.1" apply false
 }
 
 allprojects {
@@ -124,6 +125,7 @@ subprojects {
     if (!isSample) {
         apply {
             plugin(BintrayRelease.plugin)
+            plugin("org.jmailen.kotlinter")
         }
 
         configure<PublishExtension> {
