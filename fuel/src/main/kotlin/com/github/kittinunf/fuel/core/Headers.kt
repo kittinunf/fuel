e2be6cf00f08644f2d47b6b90a89e6377747239b
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-10-25 04:06:41 +0200

    Update fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Headers.kt
    Fixes the hash equals. A triple === is only true if it's the same string, completely defeating the purpose, as it never is (upcase creates a new one).
    
    Switching to collection in order to be more generous with what to accept.
    
    Adding transformIterate and helper functions to collapse the headers.

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Headers.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Headers.kt
@@ -7,11 +7,15 @@ data class HeaderName(val name: String) {
     }
 
     override fun equals(other: Any?): Boolean {
-        return (other is HeaderName) && other.normalized === normalized
+        return (other is HeaderName) && other.normalized == normalized
+    }
+
+    override fun toString(): String {
+        return name
     }
 }
 
-typealias HeaderValues = List<String>
+typealias HeaderValues = Collection<String>
 
 class Headers: MutableMap<String, HeaderValues> {
 
@@ -51,18 +55,26 @@ class Headers: MutableMap<String, HeaderValues> {
         return contents.put(HeaderName(key), value)
     }
 
-    fun appendAll(key: String, value: List<Any>) {
+    fun append(key: String, value: Collection<*>): Headers {
         val current = contents.getOrPut(HeaderName(key)) { emptyList() }
         put(key, current.plus(value.map { it.toString() }))
+        return this
     }
 
-    fun append(key: String, value: Any) {
-        val current = contents.getOrPut(HeaderName(key)) { emptyList() }
-        put(key, current.plus(value.toString()))
+    fun append(key: String, value: Any): Headers {
+        // Some headers can not be appended per the RFC
+        if (Headers.isSingleValue(HeaderName(key))) {
+            set(key, value.toString())
+            return this
+        }
+
+        put(key, this[key].plus(value.toString()))
+        return this
     }
 
-    fun replace(key: String, value: String) {
+    fun replace(key: String, value: String): Headers {
         put(key, listOf(value))
+        return this
     }
 
     /**
@@ -86,7 +98,7 @@ class Headers: MutableMap<String, HeaderValues> {
      * Returns the number of key/value pairs in the map.
      */
     override val size: Int
-       get() = contents.size
+        get() = contents.size
 
     /**
      * Returns `true` if the map contains the specified [key].
@@ -95,7 +107,7 @@ class Headers: MutableMap<String, HeaderValues> {
         return contents.containsKey(HeaderName(key))
     }
 
-    /**
+        /**
      * Returns `true` if the map maps one or more keys to the specified [value].
      */
     override fun containsValue(value: HeaderValues): Boolean {
@@ -105,16 +117,15 @@ class Headers: MutableMap<String, HeaderValues> {
     /**
      * Returns the value corresponding to the given [key], or `null` if such a key is not present in the map.
      */
-    override fun get(key: String): HeaderValues? {
-        return contents[HeaderName(key)]
+    override fun get(key: String): HeaderValues {
+        return contents[HeaderName(key)].orEmpty()
     }
 
-    operator fun set(key: String, value: String) {
-        replace(key, value)
-    }
+    operator fun set(key: String, value: String) = replace(key, value)
 
-    operator fun set(key: String, values: HeaderValues) {
+    operator fun set(key: String, values: HeaderValues): Headers {
         put(key, values)
+        return this
     }
 
     /**
@@ -124,8 +135,92 @@ class Headers: MutableMap<String, HeaderValues> {
         return contents.isEmpty()
     }
 
+    fun transformIterate(set: (key: String, value: String) -> Any, add: (key: String, value: String) -> Any = set) {
+        for ((key, values) in this) {
+            val header = HeaderName(key)
+            when (Headers.isCollapsible(header)) {
+                true ->
+                    // NOTE: HTTP requires all request properties which can
+                    //     * legally have multiple instances with the same key
+                    //     * to use a comma-separated list syntax which enables multiple
+                    //     * properties to be appended into a single property.
+                    set(key, Headers.collapse(header, values))
+                false -> when (Headers.isSingleValue(header)) {
+                    true -> values.lastOrNull()?.let { set(key, it) }
+                    false -> values.forEach { add(key, it) }
+                }
+            }
+        }
+    }
+
+    override fun toString(): String {
+        return contents.toString()
+    }
+
     companion object {
+        fun isCollapsible(key: HeaderName): Boolean {
+            return when (key) {
+                // These headers, per RFC, SHOULD NOT be collapsed into a single value
+                HeaderName(SET_COOKIE) -> false
+                else -> true
+            }
+        }
+
+        fun isSingleValue(key: HeaderName): Boolean {
+            return when(key) {
+                // These headers, per RFC, SHOULD only appear once.
+                HeaderName(AGE) -> true
+                HeaderName(CONTENT_TYPE) -> true
+                HeaderName(CONTENT_LENGTH) -> true
+                HeaderName(CONTENT_LOCATION) -> true
+                HeaderName(EXPECT) -> true
+                HeaderName(EXPIRES) -> true
+                HeaderName(LOCATION) -> true
+                else -> false
+            }
+        }
+
+        fun collapse(header: HeaderName, values: HeaderValues): String {
+            return when(header){
+                HeaderName(COOKIE) -> values.joinToString("; ")
+                else -> values.joinToString(", ")
+            }
+        }
+
+        fun from(mapOf: Map<String, Any>): Headers {
+            return mapOf.entries.fold(Headers()) { result, entry ->
+                // The issue here is that connection.headerFields is typed as Map<String, List<String>> but in fact is
+                //   a Map<String?, List<String>> and thus needs this cast here or it will break as an
+                //   java.lang.IllegalArgumentException: Parameter specified as non-null is null
+                //
+                val key = (entry.key as String?).orEmpty().ifBlank { null } ?: return@fold result
+                val value = entry.value
+
+                when (value) {
+                    is Collection<*> -> {
+                        val values = value.ifEmpty { null } ?: return@fold result
+                        result.set(key, values.map { v -> v.toString() })
+                    }
+                    else -> result.set(key, value.toString())
+                }
+
+            }
+        }
+
+        fun from(vararg pairs: Pair<String, Any>): Headers {
+            return from(pairs.toMap())
+        }
+
+        const val ACCEPT_ENCODING = "Accept-Encoding"
+        const val AGE = "Age"
         const val AUTHORIZATION = "Authorization"
+        const val CONTENT_LENGTH = "Content-Length"
+        const val CONTENT_LOCATION = "Content-Location"
         const val CONTENT_TYPE = "Content-Type"
+        const val COOKIE = "Cookie"
+        const val EXPECT = "Expect"
+        const val EXPIRES = "Expires"
+        const val LOCATION = "Location"
+        const val SET_COOKIE = "Set-Cookie"
     }
 }
