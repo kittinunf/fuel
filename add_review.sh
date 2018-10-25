70eb8463106c7e650dc77ed1574164ca522de47c
Kittinun Vantasin <kittinun.f@gmail.com>
2018-10-25 14:54:35 +0900

    Try this instead

--- add_review.sh
+++ add_review.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+rm lint.txt
+
 for file in $(find . -type f -name "*-lint.txt");
 do
   module="$(cut -d '/' -f 2 <<< "$file")"
   while read line;
   do
-    i="$module/$line"
-    $i | ./reviewdog -efm="%f:%l:%c:%m" -name="ktlint" -reporter=github-pr-review
+    i="$(pwd)/$module/$line"
+    echo "$i" >> lint.txt
   done < $file
 done
+
+#cat lint.txt | ./reviewdog -efm="%f:%l:%c: %m" -name="ktlint" -diff="git diff master"
+cat lint.txt | ./reviewdog -efm="%f:%l:%c: %m" -name="ktlint" -reporter=github-pr-review
