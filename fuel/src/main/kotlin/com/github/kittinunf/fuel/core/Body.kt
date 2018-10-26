d17bf698439c43fb4140a21c372df527ba844c87
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-10-26 05:13:59 +0200

    Turn into an interface
    This allows us to actually define different type of bodies, as well as extending a body to support #464. The length is now also a lazy call, so that it may be calculated ONLY if necessary.

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Body.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Body.kt
@@ -3,45 +3,60 @@ package com.github.kittinunf.fuel.core
 import java.io.ByteArrayOutputStream
 import java.io.OutputStream
 import java.io.Reader
-import java.io.Writer
 import java.nio.charset.Charset
 
 typealias BodySource = (() -> Reader)
+typealias BodyLength = (() -> Number)
 
-data class Body(
+interface Body {
+    fun toByteArray() : ByteArray
+    fun writeTo(outputStream: OutputStream, charset: Charset? = null)
+    fun isEmpty() : Boolean
+    fun isNotEmpty() : Boolean
+
+    val length: Number?
+}
+
+data class DefaultBody(
     private var openReader: BodySource? = null,
-    val length: Number? = null,
+    private var calculateLength: BodyLength? = null,
     val charset: Charset = Charsets.UTF_8
-) {
-    fun toByteArray(): ByteArray {
-        return ByteArrayOutputStream(length?.toInt() ?: 32).apply {
-            writeTo(this)
-        }.toByteArray()
-    }
-
-    fun writeTo(outputStream: OutputStream) {
-        writeTo(outputStream.writer(charset))
-    }
-
-    fun writeTo(outputStream: Writer) {
-        outputStream.buffered().apply {
-            val reader = openReader!!.invoke().buffered()
-            reader.copyTo(this)
-            reader.close()
+) : Body {
+    override fun toByteArray(): ByteArray {
+        return ByteArrayOutputStream(length?.toInt() ?: 32).let {
+            writeTo(it, charset)
+            it.close()
+            it.toByteArray()
         }
     }
 
-    fun isEmpty(): Boolean {
+    override fun writeTo(outputStream: OutputStream, charset: Charset?) {
+        // This actually writes whatever the body is outputting with the given charset.
+        outputStream.bufferedWriter(charset ?: this.charset).apply {
+            val reader = openReader!!.invoke().buffered()
+            reader.copyTo(this)
+            reader.close()
+
+            // The outputStream is buffered, but we are done reading, so it's time to flush what's left
+            flush()
+        }
+    }
+
+    override fun isEmpty(): Boolean {
         return openReader == null  || (length != null && length == 0)
     }
 
-    fun isNotEmpty(): Boolean = !isEmpty()
+    override fun isNotEmpty(): Boolean = !isEmpty()
+
+    override val length: Number? by lazy {
+        calculateLength?.invoke()
+    }
 
     companion object {
-        fun from(openReader: BodySource, length: Number?, charset: Charset = Charsets.UTF_8): Body {
-            return Body(
+        fun from(openReader: BodySource, calculateLength: BodyLength?, charset: Charset = Charsets.UTF_8): Body {
+            return DefaultBody(
                 openReader = openReader,
-                length = length,
+                calculateLength = calculateLength,
                 charset = charset
             )
         }
