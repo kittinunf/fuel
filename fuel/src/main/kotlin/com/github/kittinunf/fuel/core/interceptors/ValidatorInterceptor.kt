de9767237d641ce0162295d26445f6e950b0c81e
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-10-26 18:11:59 +0200

    Inverse condition to have common path first

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/interceptors/ValidatorInterceptor.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/interceptors/ValidatorInterceptor.kt
@@ -8,11 +8,14 @@ import com.github.kittinunf.fuel.core.Response
 fun validatorResponseInterceptor(validRange: IntRange) =
         { next: (Request, Response) -> Response ->
             { request: Request, response: Response ->
-                if (!validRange.contains(response.statusCode)) {
-                    val error = FuelError(HttpException(response.statusCode, response.responseMessage), response.data, response)
-                    throw error
-                } else {
+                if (validRange.contains(response.statusCode)) {
                     next(request, response)
+                } else {
+                    throw FuelError(
+                        HttpException(response.statusCode, response.responseMessage),
+                        response.data,
+                        response
+                    )
                 }
             }
         }
