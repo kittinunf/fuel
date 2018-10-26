eff9fcd104c827a9c78b2874f1f1476aee4f966e
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-10-26 18:08:48 +0200

    Make bodies be consumable at most once
    This solves multiple issues with input streams being read twice, when logging is turned on. Before, toByteArray would load into memory and that was that. Now it reads directly from an inputstream. However, most streams are only consumable once, thus this solves that.
    
    When the stream is read into memory we might as well keep it around.

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Body.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Body.kt
@@ -1,6 +1,8 @@
 package com.github.kittinunf.fuel.core
 
+import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.InputStreamReader
 import java.io.OutputStream
 import java.io.Reader
 import java.nio.charset.Charset
@@ -12,7 +14,7 @@ interface Body {
     fun toByteArray() : ByteArray
     fun writeTo(outputStream: OutputStream, charset: Charset? = null)
     fun isEmpty() : Boolean
-    fun isNotEmpty() : Boolean
+    fun isConsumed(): Boolean
 
     val length: Number?
 }
@@ -22,15 +24,22 @@ data class DefaultBody(
     private var calculateLength: BodyLength? = null,
     val charset: Charset = Charsets.UTF_8
 ) : Body {
+
     override fun toByteArray(): ByteArray {
         if (isEmpty()) {
             return ByteArray(0)
         }
 
+        // Stream may be completely consumed by writeTo. Therefore there is no guarantee that the stream will be
+        // available a second time
+
         return ByteArrayOutputStream(length?.toInt() ?: 32).let {
             writeTo(it, charset)
             it.close()
             it.toByteArray()
+        }.apply {
+            openReader = { InputStreamReader(ByteArrayInputStream(this)) }
+            calculateLength = { this.size }
         }
     }
 
@@ -38,26 +47,32 @@ data class DefaultBody(
         // This actually writes whatever the body is outputting with the given charset.
         outputStream.bufferedWriter(charset ?: this.charset).apply {
             val reader = openReader!!.invoke().buffered()
-            println("[Body] got reader: $reader")
             reader.copyTo(this)
             reader.close()
 
             // The outputStream is buffered, but we are done reading, so it's time to flush what's left
             flush()
+
+            // This prevents implementations from writing the body twice
+            openReader = CONSUMED_READER
         }
     }
 
-    override fun isEmpty(): Boolean {
-        return openReader == null  || (length != null && length == 0)
-    }
-
-    override fun isNotEmpty(): Boolean = !isEmpty()
+    override fun isEmpty() = openReader == null  || (length != null && length == 0)
+    override fun isConsumed() = openReader === CONSUMED_READER
 
     override val length: Number? by lazy {
         calculateLength?.invoke()
     }
 
     companion object {
+
+        val CONSUMED_READER = {
+            throw IllegalStateException(
+                "The input has already been written to an output stream and can not be consumed again."
+            )
+        }
+
         fun from(openReader: BodySource, calculateLength: BodyLength?, charset: Charset = Charsets.UTF_8): Body {
             return DefaultBody(
                 openReader = openReader,
