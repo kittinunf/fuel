594d7fac9ae47719572c8f63733d85a1fc5b3344
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-10-28 02:11:57 +0100

    Dedicated Progress handler
    This progress handler allows numerous handlers to be registered, all at the same time

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Progress.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Progress.kt
@@ -2,5 +2,28 @@ package com.github.kittinunf.fuel.core
 
 typealias ProgressCallback = (readBytes: Long, totalBytes: Long) -> Unit
 
-// placeholder
-class Progress
+data class Progress(val handlers: MutableCollection<ProgressCallback> = mutableListOf()) {
+    fun add(handler: ProgressCallback): Progress {
+        plusAssign(handler)
+        return this
+    }
+
+    fun remove(handler: ProgressCallback): Progress {
+        minusAssign(handler)
+        return this
+    }
+
+    fun invoke(readBytes: Long, totalBytes: Long) {
+        handlers.forEach {
+            it.invoke(readBytes, totalBytes)
+        }
+    }
+
+    operator fun plusAssign(handler: ProgressCallback) {
+        handlers.add(handler)
+    }
+
+    operator fun minusAssign(handler: ProgressCallback) {
+        handlers.remove(handler)
+    }
+}
