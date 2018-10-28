715dc993c057014757ec55c01c452f66e43dad27
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-10-28 02:13:27 +0100

    Have generic dataparts using streams
    Should work for #344
    Should work for #479

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/DataPart.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/DataPart.kt
@@ -1,9 +1,72 @@
 package com.github.kittinunf.fuel.core
 
 import java.io.File
+import java.io.FileInputStream
+import java.nio.charset.Charset
+
+/**
+ * https://tools.ietf.org/html/rfc2183
+ */
+enum class ContentDispositionType(val value: String) {
+    INLINE("inline"),
+    ATTACHMENT("attachment"),
+    FORM_DATA("form-data")
+}
 
 data class DataPart(
-    val file: File,
-    val name: String = file.nameWithoutExtension,
-    val type: String = ""
-)
+    val partName: String,
+    val fileName: String?,
+    val contentType: String?,
+    val type: ContentDispositionType,
+    val dataPartBody: Body
+) : Body by dataPartBody {
+    companion object {
+        val DEFAULT_CONTENT_TYPE = "application/octet-stream"
+
+        fun from(
+            file: File,
+            length: Number? = null,
+            partName: String? = null,
+            fileName: String? = null,
+            contentType: String? = null,
+            charset: Charset = Charsets.UTF_8
+        ): DataPart {
+
+            val calculateLength = when (charset == Charsets.UTF_8) {
+                true -> length?.let { { it } } ?: { file.length() }
+                false -> null
+            }
+
+            return from(
+                partName = partName ?: file.nameWithoutExtension,
+                fileName = fileName ?: file.name,
+                contentType = contentType,
+                dataPartBody = DefaultBody.from(
+                    openStream = { FileInputStream(file) },
+                    calculateLength = calculateLength,
+                    charset = charset
+                )
+            )
+        }
+
+        fun from(partName: String, fileName: String? = null, contentType: String? = null, dataPartBody: Body) = DataPart(
+            partName = partName,
+            fileName = fileName,
+            contentType = contentType,
+            type = ContentDispositionType.FORM_DATA,
+            dataPartBody = dataPartBody
+        )
+
+        fun from(blob: Blob, fileName: String? = null, contentType: String? = null, charset: Charset = Charsets.UTF_8) = DataPart(
+            partName = blob.name,
+            fileName = fileName,
+            contentType = contentType,
+            type = ContentDispositionType.FORM_DATA,
+            dataPartBody = DefaultBody.from(
+                openStream = { blob.inputStream.invoke() },
+                calculateLength = { blob.length },
+                charset = charset
+            )
+        )
+    }
+}
