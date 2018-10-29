0018ab2d349a089bae83abf0def5f8c50f1496ff
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-10-29 23:00:05 +0100

    KDoc for Body interface

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Body.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/Body.kt
@@ -10,13 +10,54 @@ typealias BodySource = (() -> InputStream)
 typealias BodyLength = (() -> Number)
 
 interface Body {
+    /**
+     * Returns the body as a [ByteArray].
+     *
+     * @note Because the body needs to be read into memory anyway, implementations may choose to make the [Body]
+     *  readable once more after calling this method, with the original [InputStream] being closed (and release its
+     *  resources). This also means that if an implementation choose to keep it around, `isConsumed` returns false.
+     *
+     * @return the entire body
+     */
     fun toByteArray(): ByteArray
+
+    /**
+     * Returns the body as an [InputStream].
+     *
+     * @note callers are responsible for closing the returned stream.
+     * @note implementations may choose to make the [Body] `isConsumed` and can not be written or read from again.
+     *
+     * @return the body as input stream
+     */
     fun toStream(): InputStream
 
-    fun writeTo(outputStream: OutputStream, charset: Charset? = null)
+    /**
+     * Writes the body to the [OutputStream].
+     *
+     * @note callers are responses for closing the [OutputStream].
+     * @note implementations may choose to make the [Body] `isConsumed` and can not be written or read from again.
+     * @note implementations are recommended to buffer the output stream if they can't ensure bulk writing.
+     *
+     * @param outputStream [OutputStream] the stream to write to
+     */
+    fun writeTo(outputStream: OutputStream)
+
+    /**
+     * Returns the body emptiness.
+     * @return [Boolean] if true, this body is empty
+     */
     fun isEmpty(): Boolean
+
+    /**
+     * Returns if the body is consumed.
+     * @return [Boolean] if true, `writeTo`, `toStream` and `toByteArray` may throw
+     */
     fun isConsumed(): Boolean
 
+    /**
+     * Returns the length of the body in bytes
+     * @return [Number?] the length in bytes, null if it is unknown
+     */
     val length: Number?
 }
 
