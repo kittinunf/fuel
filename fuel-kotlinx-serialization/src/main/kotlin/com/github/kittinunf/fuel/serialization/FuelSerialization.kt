8bc4bc43db42d77d0dbb1410141cb8eb541fdf8a
NikkyAI <root@nikky.moe>
2018-11-02 17:31:47 +0100

    add option to pass in a Serializer to kotlinxSerializerOf
    this also avoids infecting user code with `@ImplicitReflectionSerializer`
    
    annoyingly the annotation requires to create extra functions
    
    💅 lint it

--- fuel-kotlinx-serialization/src/main/kotlin/com/github/kittinunf/fuel/serialization/FuelSerialization.kt
+++ fuel-kotlinx-serialization/src/main/kotlin/com/github/kittinunf/fuel/serialization/FuelSerialization.kt
@@ -14,28 +14,44 @@ import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.json.JSON
 import kotlinx.serialization.serializer
 
-@ImplicitReflectionSerializer
 inline fun <reified T : Any> Request.responseObject(
-    loader: DeserializationStrategy<T> = T::class.serializer(),
+    loader: DeserializationStrategy<T>,
     json: JSON = JSON.plain,
     noinline deserializer: (Request, Response, Result<T, FuelError>) -> Unit
 ) = response(kotlinxDeserializerOf(loader, json), deserializer)
 
 @ImplicitReflectionSerializer
 inline fun <reified T : Any> Request.responseObject(
+    json: JSON = JSON.plain,
+    noinline deserializer: (Request, Response, Result<T, FuelError>) -> Unit
+) = responseObject(T::class.serializer(), json, deserializer)
+
+inline fun <reified T : Any> Request.responseObject(
     deserializer: Handler<T>,
-    loader: DeserializationStrategy<T> = T::class.serializer(),
+    loader: DeserializationStrategy<T>,
     json: JSON = JSON.plain
 ) = response(kotlinxDeserializerOf(loader, json), deserializer)
 
 @ImplicitReflectionSerializer
 inline fun <reified T : Any> Request.responseObject(
-    loader: DeserializationStrategy<T> = T::class.serializer(),
+    deserializer: Handler<T>,
+    json: JSON = JSON.plain
+) = responseObject(deserializer, T::class.serializer(), json)
+
+inline fun <reified T : Any> Request.responseObject(
+    loader: DeserializationStrategy<T>,
     json: JSON = JSON.plain
 ) = response(kotlinxDeserializerOf(loader, json))
 
 @ImplicitReflectionSerializer
-inline fun <reified T : Any> kotlinxDeserializerOf(loader: DeserializationStrategy<T> = T::class.serializer(), json: JSON = JSON.plain) = object : ResponseDeserializable<T> {
+inline fun <reified T : Any> Request.responseObject(
+    json: JSON = JSON.plain
+) = responseObject(T::class.serializer(), json)
+
+inline fun <reified T : Any> kotlinxDeserializerOf(
+    loader: DeserializationStrategy<T>,
+    json: JSON = JSON.plain
+) = object : ResponseDeserializable<T> {
     override fun deserialize(content: String): T? = json.parse(loader, content)
     override fun deserialize(reader: Reader): T? = deserialize(reader.readText())
     override fun deserialize(bytes: ByteArray): T? = deserialize(String(bytes))
@@ -46,3 +62,8 @@ inline fun <reified T : Any> kotlinxDeserializerOf(loader: DeserializationStrate
         }
     }
 }
+
+@ImplicitReflectionSerializer
+inline fun <reified T : Any> kotlinxDeserializerOf(
+    json: JSON = JSON.plain
+) = kotlinxDeserializerOf(T::class.serializer(), json)
