e4fda93b9199708f48f6a09bcfad30aadaf0240a
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-11-07 12:39:48 +0100

    Add kdoc for extensions

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/util/DecodeStream.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/util/DecodeStream.kt
@@ -9,6 +9,13 @@ private val UNSUPPORTED_DECODE_ENCODING: DecodeFallbackCallback = { _, encoding 
     throw UnsupportedOperationException("Decoding $encoding is not supported. Expected one of gzip, deflate, identity.")
 }
 
+/**
+ * Return an [InputStream] that decodes [encoding] when it's read.
+ *
+ * @param encoding [String] the encoding ("gzip", "deflate", "chunked" or "identity")
+ * @param unsupported [DecodeFallbackCallback] fallback callback that is called if encoding is not supported natively
+ * @return [InputStream] the wrapped [InputStream] that is decoded when it's being read
+ */
 fun InputStream.decode(encoding: String, unsupported: DecodeFallbackCallback = UNSUPPORTED_DECODE_ENCODING) =
     when (encoding.trim()) {
         "gzip" -> GZIPInputStream(this)
@@ -18,5 +25,15 @@ fun InputStream.decode(encoding: String, unsupported: DecodeFallbackCallback = U
         else -> unsupported(this, encoding)
     }
 
+/**
+ * Return an [InputStream] that decodes all [encodings] when it's read. They should be ordered from inner to outer, in
+ * the order they were applied. A stream that was first deflated and then gzipped should be ("deflate", "gzip").
+ *
+ * @see decode
+ *
+ * @param encodings [Iterable<String>] all the encodings that should be decoded
+ * @param unsupported [DecodeFallbackCallback] fallback callback that is called if encoding is not supported natively
+ * @return [InputStream] the wrapped [InputStream] that is decoded when it's being read
+ */
 fun InputStream.decode(encodings: Iterable<String>, unsupported: DecodeFallbackCallback = UNSUPPORTED_DECODE_ENCODING) =
     encodings.fold(this) { stream, encoding -> stream.decode(encoding, unsupported)}
--- fuel/src/main/kotlin/com/github/kittinunf/fuel/util/EncodeStream.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/util/EncodeStream.kt
@@ -4,11 +4,18 @@ import java.io.OutputStream
 import java.util.zip.DeflaterOutputStream
 import java.util.zip.GZIPOutputStream
 
+/**
+ * Encodes the [OutputStream] using [encoding]
+ *
+ * @param encoding [String] the encoding to apply, one of ("gzip", "deflate" or "identity")
+ * @param unsupported fallback callback that is called if encoding is not supported natively
+ * @return [OutputStream] the wrapped [OutputStream] that is encoded when it's written to
+ */
 fun OutputStream.encode(
-        encoding: String,
-        unsupported: (OutputStream, String) -> OutputStream = { _, _ ->
-            throw UnsupportedOperationException("Encoding $encoding is not supported. Expected one of gzip, deflate, identity.")
-        }
+    encoding: String,
+    unsupported: (OutputStream, String) -> OutputStream = { _, _ ->
+        throw UnsupportedOperationException("Encoding $encoding is not supported. Expected one of gzip, deflate, identity.")
+    }
 ) = when(encoding.trim()) {
     "gzip" -> GZIPOutputStream(this)
     "deflate", "inflate" -> DeflaterOutputStream(this)
