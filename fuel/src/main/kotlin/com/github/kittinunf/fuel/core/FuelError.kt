9109cc83d2e5ee8f9cbf93db2a1136791b930457
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-11-07 21:30:41 +0100

    Allow wrapping in FuelError
    Many times we try to cast exceptions into FuelErrors and then wrap them if it fails. This moves that logic to the FuelError class.

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/FuelError.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/FuelError.kt
@@ -1,9 +1,20 @@
 package com.github.kittinunf.fuel.core
 
-class FuelError(val exception: Exception, val errorData: ByteArray = ByteArray(0), val response: Response = Response.error())
-    : Exception(exception) {
+class FuelError(
+    val exception: Throwable,
+    val errorData: ByteArray = ByteArray(0),
+    val response: Response = Response.error()
+) : Exception(exception) {
     init {
         stackTrace = exception.stackTrace
     }
+
     override fun toString(): String = "${exception.javaClass.canonicalName}: ${exception.message ?: "<no message>"}"
+
+    companion object {
+        fun wrap(it: Throwable): FuelError = when(it) {
+            is FuelError -> it
+            else -> FuelError(it)
+        }
+    }
 }
