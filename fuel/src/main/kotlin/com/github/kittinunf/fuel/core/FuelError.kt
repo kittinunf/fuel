12af0925eac96cf84188b6ec2bd3d2be171be12e
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2018-11-08 20:49:18 +0100

    Rich error stack traces and data handling
    Expect the fuel errors to be rewrapped when it's recatched. This allows us to see all the bubbles (rethrows). The original error that yielded the fuel error is correctly stored (as the cause) and listed when the error is printed or stringified. For each consecutive throw, it only adds the new stack trace elements, which is great with asynchronous calls, as threads have their own stack.
    
    Additionally adds explicit "Bubble" exception, and uses property getters instead of explicit passing. We know what response is.

--- fuel/src/main/kotlin/com/github/kittinunf/fuel/core/FuelError.kt
+++ fuel/src/main/kotlin/com/github/kittinunf/fuel/core/FuelError.kt
@@ -1,19 +1,54 @@
 package com.github.kittinunf.fuel.core
 
-class FuelError(
-    val exception: Throwable,
-    val errorData: ByteArray = ByteArray(0),
+import java.io.InterruptedIOException
+
+private class BubbleFuelError(inner: FuelError) : FuelError(inner, inner.response)
+
+open class FuelError internal constructor(
+    exception: Throwable,
     val response: Response = Response.error()
-) : Exception(exception) {
+) : Exception(exception.message, exception) {
     init {
-        stackTrace = exception.stackTrace
+        // Only store new stack since `cause`
+        stackTrace = stackTrace.takeWhile { stack -> exception.stackTrace.find { inner -> inner == stack } == null }
+            .toTypedArray()
     }
 
-    override fun toString(): String = "${exception.javaClass.canonicalName}: ${exception.message ?: "<no message>"}"
+    override fun toString(): String = this::class.java.canonicalName.plus(buildString {
+        appendln(": $message")
+        stackTrace.forEach { stack -> appendln("\t$stack") }
+
+        cause?.also {
+            append("Caused by: ")
+            appendln(it.toString())
+            when(it) {
+                is FuelError -> {}
+                else -> { it.stackTrace.forEach { stack -> appendln("\t$stack") } }
+            }
+        }
+    })
+
+    val exception: Throwable get() {
+        var pointer: Throwable = this
+        while(pointer is FuelError && pointer.cause != null) {
+            pointer = pointer.cause!!
+        }
+
+        return pointer
+    }
+
+    val errorData: ByteArray get() = response.data
+
+    /**
+     * This FuelError was caused by an interruption
+     */
+    val causedByInterruption: Boolean get() =
+        exception is InterruptedException ||
+        exception is InterruptedIOException
 
     companion object {
         fun wrap(it: Throwable, response: Response = Response.error()): FuelError = when (it) {
-            is FuelError -> it
+            is FuelError -> BubbleFuelError(it)
             else -> FuelError(it, response = response)
         }
     }
