90300ac0b3cbf5eec81752341f9b6247901230fd
Tan Jun Rong <tanjunrong@gmail.com>
2018-11-19 23:56:46 +0900

    remove unnecessary dependency from fuel

--- fuel/build.gradle.kts
+++ fuel/build.gradle.kts
@@ -2,8 +2,6 @@ plugins { java }
 
 dependencies {
     compile(Result.dependency)
-    compile(Stetho.plugin)
-    compile(Stetho.StethoUrlConnection.plugin)
     testCompile(Json.dependency)
     testCompile(project(":fuel-test"))
 }
