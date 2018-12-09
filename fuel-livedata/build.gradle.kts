09d169b7ec127bfa404c53279e46d6cba7a2cc99
Jonathan Steele <xfsunoles@gmail.com>
2018-12-08 22:34:47 -0500

    Migrate to Android API Level 28 and AndroidX

--- fuel-livedata/build.gradle.kts
+++ fuel-livedata/build.gradle.kts
@@ -1,7 +1,7 @@
 dependencies {
     api(project(":fuel"))
     implementation(Kotlin.stdlib)
-    implementation(Android.Arch.extensions)
+    implementation(AndroidX.Arch.extensions)
     testImplementation(RoboElectric.dependency)
     testImplementation(JUnit.dependency)
     testImplementation(project(":fuel-test"))
--- sample/src/main/kotlin/com/example/fuel/MainActivity.kt
+++ sample/src/main/kotlin/com/example/fuel/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.fuel
 
 import android.os.Bundle
 import android.os.Handler
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelError
@@ -173,7 +173,7 @@ class MainActivity : AppCompatActivity() {
     private fun httpDownload() {
         val n = 100
         Fuel.download("/bytes/${1024 * n}")
-            .destination { _, _ -> File(filesDir, "test.tmp") }
+            .fileDestination { _, _ -> File(filesDir, "test.tmp") }
             .progress { readBytes, totalBytes ->
                 val progress = "$readBytes / $totalBytes"
                 runOnUiThread { mainAuxText.text = progress }
@@ -210,13 +210,13 @@ class MainActivity : AppCompatActivity() {
             .authentication()
             .basic(username, password)
             .also { Log.d(TAG, it.cUrlString()) }
-            .responseString { request, _, result -> update(result) }
+            .responseString { _, _, result -> update(result) }
 
         "/basic-auth/$username/$password".httpGet()
             .authentication()
             .basic(username, password)
             .also { Log.d(TAG, it.cUrlString()) }
-            .responseString { request, _, result -> update(result) }
+            .responseString { _, _, result -> update(result) }
     }
 
     private fun httpRxSupport() {
