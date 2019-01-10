8f9a1ec5b21ce6f2ebc1a988d0f2224553744f45
Matteo Crippa <matteo@boostco.de>
2019-01-11 00:08:07 +0100

    1.1.17

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 // Library version
 object Fuel {
-    const val publishVersion = "1.16.0"
+    const val publishVersion = "1.17.0"
     const val groupId = "com.github.kittinunf.fuel"
 
     const val compileSdkVersion = 28
