df3dcef6a3ca874355574b2614c7f492e7076178
Kittinun Vantasin <kittinun.f@gmail.com>
2019-01-25 01:11:18 +0900

    Update to version 2.0.0

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 // Library version
 object Fuel {
-    const val publishVersion = "1.16.0"
+    const val publishVersion = "2.0.0"
     const val groupId = "com.github.kittinunf.fuel"
 
     const val compileSdkVersion = 28
