63c5f4cb181e62fe5188d3de164712054c64d194
Guilherme Pohlmann da Rosa <guilherme.pr@outlook.com>
2019-01-24 08:56:00 -0200

    :sparkles: [fuel-jackson] Added custom mapper support. (#569)

--- fuel-jackson/README.md
+++ fuel-jackson/README.md
@@ -17,4 +17,58 @@ compile 'com.github.kittinunf.fuel:fuel-jackson:<latest-version>'
 
 ## Usage
 
-See `Fueljackson.kt`
+The Fuel-Jackson module provides a built in support for Jackson deserialization.
+This is done by adding the `responseObject` extension function into Fuel `Request` interface.
+
+By default, the `responseObject` call will use the `defaultMapper` property defined in `FuelJackson.kt`.
+
+```kotlin
+data class HttpBinUserAgentModel(var userAgent: String = "")
+
+Fuel.get("/user-agent")
+    .responseObject<HttpBinUserAgentModel>()
+```
+
+Alternatively, you can provide your own `ObjectMapper` as a parameter to it.
+
+```kotlin
+data class HttpBinUserAgentModel(var userAgent: String = "")
+
+val mapper = ObjectMapper().registerKotlinModule()
+                           .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+mapper.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+
+Fuel.get("/user-agent")
+    .responseObject<HttpBinUserAgentModel>(mapper)
+```
+
+Also, the `responseObject` overloads allows you to pass `Response Handlers` as lambda functions
+
+```kotlin
+data class HttpBinUserAgentModel(var userAgent: String = "")
+
+Fuel.get("/user-agent")
+    .responseObject<HttpBinUserAgentModel> { request, response, result ->
+                    //handle here
+                }
+```
+
+or `ResponseHandler<T>` instances.
+
+```kotlin
+data class HttpBinUserAgentModel(var userAgent: String = "")
+
+Fuel.get("/user-agent")
+    .responseObject(object : ResponseHandler<HttpBinUserAgentModel> {
+           override fun success(request: Request, response: Response, value: HttpBinUserAgentModel) {
+              //handle success
+           }
+    
+           override fun failure(request: Request, response: Response, error: FuelError) {
+              //handle failure
+           }
+     })
+```
+
+Both overloads allows you to provide custom `ObjectMapper` if needed.
--- fuel-jackson/src/test/kotlin/com/github/kittinunf/fuel/FuelJacksonTest.kt
+++ fuel-jackson/src/test/kotlin/com/github/kittinunf/fuel/FuelJacksonTest.kt
@@ -1,5 +1,9 @@
 package com.github.kittinunf.fuel
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.core.ResponseHandler
 import com.github.kittinunf.fuel.core.Request
@@ -41,6 +45,21 @@ class FuelJacksonTest : MockHttpTestCase() {
     }
 
     @Test
+    fun jacksonTestResponseObjectWithCustomMapper() {
+        mock.chain(
+                request = mock.request().withPath("/user-agent"),
+                response = mock.reflect()
+        )
+
+        Fuel.get(mock.path("user-agent"))
+                .responseObject(jacksonDeserializerOf<HttpBinUserAgentModel>(createCustomMapper())) { _, _, result ->
+                    assertThat(result.component1(), instanceOf(HttpBinUserAgentModel::class.java))
+                    assertThat(result.component1()?.userAgent, not(""))
+                    assertThat(result.component2(), instanceOf(FuelError::class.java))
+                }
+    }
+
+    @Test
     fun jacksonTestResponseObjectError() {
         mock.chain(
             request = mock.request().withPath("/user-agent"),
@@ -55,6 +74,20 @@ class FuelJacksonTest : MockHttpTestCase() {
     }
 
     @Test
+    fun jacksonTestResponseObjectErrorWithCustomMapper() {
+        mock.chain(
+                request = mock.request().withPath("/user-agent"),
+                response = mock.response().withStatusCode(HttpURLConnection.HTTP_NOT_FOUND)
+        )
+
+        Fuel.get(mock.path("user-agent"))
+                .responseObject(jacksonDeserializerOf<HttpBinUserAgentModel>(createCustomMapper())) { _, _, result ->
+                    assertThat(result.component1(), notNullValue())
+                    assertThat(result.component2(), instanceOf(Result.Failure::class.java))
+                }
+    }
+
+    @Test
     fun jacksonTestResponseDeserializerObject() {
         mock.chain(
             request = mock.request().withPath("/user-agent"),
@@ -66,6 +99,20 @@ class FuelJacksonTest : MockHttpTestCase() {
                 assertThat(result.component1(), notNullValue())
                 assertThat(result.component2(), notNullValue())
             }
+    }
+
+    @Test
+    fun jacksonTestResponseDeserializerObjectWithCustomMapper() {
+        mock.chain(
+                request = mock.request().withPath("/user-agent"),
+                response = mock.reflect()
+        )
+
+        Fuel.get(mock.path("user-agent"))
+                .responseObject<HttpBinUserAgentModel>(createCustomMapper()) { _, _, result ->
+                    assertThat(result.component1(), notNullValue())
+                    assertThat(result.component2(), notNullValue())
+                }
     }
 
     @Test
@@ -102,6 +149,25 @@ class FuelJacksonTest : MockHttpTestCase() {
     }
 
     @Test
+    fun jacksonTestResponseHandlerObjectWithCustomMapper() {
+        mock.chain(
+                request = mock.request().withPath("/user-agent"),
+                response = mock.reflect()
+        )
+
+        Fuel.get(mock.path("user-agent"))
+                .responseObject(createCustomMapper(), object : ResponseHandler<HttpBinUserAgentModel> {
+                    override fun success(request: Request, response: Response, value: HttpBinUserAgentModel) {
+                        assertThat(value, notNullValue())
+                    }
+
+                    override fun failure(request: Request, response: Response, error: FuelError) {
+                        assertThat(error, notNullValue())
+                    }
+                })
+    }
+
+    @Test
     fun jacksonTestResponseHandlerObjectError() {
         mock.chain(
             request = mock.request().withPath("/user-agent"),
@@ -109,7 +175,7 @@ class FuelJacksonTest : MockHttpTestCase() {
         )
 
         Fuel.get(mock.path("user-agent"))
-            .responseObject(object : ResponseHandler<HttpBinUserAgentModel> {
+            .responseObject(createCustomMapper(), object : ResponseHandler<HttpBinUserAgentModel> {
                 override fun success(request: Request, response: Response, value: HttpBinUserAgentModel) {
                     assertThat(value, notNullValue())
                 }
@@ -137,6 +203,23 @@ class FuelJacksonTest : MockHttpTestCase() {
     }
 
     @Test
+    fun jacksonTestResponseSyncObjectWithCustomMapper() {
+        mock.chain(
+                request = mock.request().withPath("/issues/1"),
+                response = mock.response().withBody(
+                        "{ \"id\": 1, \"title\": \"issue 1\", \"number\": null, \"snake_property\": 10 }"
+                ).withStatusCode(HttpURLConnection.HTTP_OK)
+        )
+
+        val (_, res, result) = Fuel.get(mock.path("issues/1")).responseObject<IssueInfo>(createCustomMapper())
+        assertThat(res, notNullValue())
+        assertThat(result.get(), notNullValue())
+        assertThat(result.get(), isA(IssueInfo::class.java))
+        assertThat(result.get().snakeProperty, equalTo(10))
+        assertThat(result, notNullValue())
+    }
+
+    @Test
     fun jacksonTestResponseSyncObjectError() {
         mock.chain(
             request = mock.request().withPath("/issues/1"),
@@ -151,7 +234,7 @@ class FuelJacksonTest : MockHttpTestCase() {
         assertThat((error as FuelError).response.statusCode, equalTo(HttpURLConnection.HTTP_NOT_FOUND))
     }
 
-    data class IssueInfo(val id: Int, val title: String, val number: Int)
+    data class IssueInfo(val id: Int, val title: String, val number: Int, val snakeProperty: Int)
 
     @Test
     fun testProcessingGenericList() {
@@ -205,5 +288,14 @@ class FuelJacksonTest : MockHttpTestCase() {
             val issueList = jacksonDeserializerOf<List<IssueInfo>>().deserialize(result.get())!!
             assertThat(issueList[0], isA(IssueInfo::class.java))
         }
+    }
+
+    private fun createCustomMapper(): ObjectMapper {
+        val mapper = ObjectMapper().registerKotlinModule()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+        mapper.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+
+        return mapper
     }
 }
