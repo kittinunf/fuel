e8b5c7912311ff1993f0b89908817618dacc3e12
Kittinun Vantasin <kittinun.f@gmail.com>
2019-09-06 15:12:04 +0900

    [Release] Version 2.2.1 (#684)

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,6 +1,6 @@
 // Library version
 object Fuel {
-    const val publishVersion = "2.2.0"
+    const val publishVersion = "2.2.1"
     const val groupId = "com.github.kittinunf.fuel"
 
     const val compileSdkVersion = 28
@@ -63,7 +63,7 @@ object Fuel {
 
 // Core dependencies
 object Kotlin {
-    const val version = "1.3.30"
+    const val version = "1.3.50"
     const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
     const val plugin = "kotlin"
     const val androidPlugin = "kotlin-android"
