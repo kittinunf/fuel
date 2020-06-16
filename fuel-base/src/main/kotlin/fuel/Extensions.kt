bd1c581faf68fbca168047d599332d04f4c1f252
iNoles <iNoles@users.noreply.github.com>
2020-06-16 02:25:03 -0400

    Remove ContinuationCallback.kt and putting codes into the Extensions.kt

--- fuel-base/src/main/kotlin/fuel/Extensions.kt
+++ fuel-base/src/main/kotlin/fuel/Extensions.kt
@@ -4,14 +4,34 @@ package fuel
 
 import kotlinx.coroutines.suspendCancellableCoroutine
 import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.Headers
 import okhttp3.Request
 import okhttp3.Response
+import java.io.IOException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
+/**
+ * Suspend extension that allows suspend [Call] inside coroutine.
+ *
+ * @return Response of response or throw exception
+ */
 internal suspend fun Call.await(): Response = suspendCancellableCoroutine { continuation ->
-    val callback = ContinuationCallback(this, continuation)
-    enqueue(callback)
-    continuation.invokeOnCancellation(callback)
+    continuation.invokeOnCancellation {
+        cancel()
+    }
+    enqueue(object : Callback {
+        override fun onResponse(call: Call, response: Response) {
+            continuation.resume(response)
+        }
+
+        override fun onFailure(call: Call, e: IOException) {
+            // Don't bother with resuming the continuation if it is already cancelled.
+            if (continuation.isCancelled) return
+            continuation.resumeWithException(e)
+        }
+    })
 }
 
 /**
