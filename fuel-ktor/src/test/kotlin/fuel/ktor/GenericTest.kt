a61c79d0aa051bafe9437557c397a7fff25a405a
iNoles <iNoles@users.noreply.github.com>
2020-07-24 20:30:56 -0400

    Added tests for Ktor's HttpTimeout feature

--- fuel-ktor/src/test/kotlin/fuel/ktor/GenericTest.kt
+++ fuel-ktor/src/test/kotlin/fuel/ktor/GenericTest.kt
@@ -1,19 +1,69 @@
 package fuel.ktor
 
 import io.ktor.client.HttpClient
-import org.junit.Assert
+import io.ktor.client.features.HttpTimeout
+import io.ktor.client.features.timeout
+import io.ktor.client.request.get
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
 import org.junit.Test
+import java.lang.IllegalArgumentException
 
 class GenericTest {
     @Test
     fun `HttpClient without parameter for not null`() {
         val httpClient = HttpClient()
-        Assert.assertNotNull(httpClient)
+        assertNotNull(httpClient)
     }
 
     @Test
     fun `get container name`() {
         val fuelEngineContainer = FuelEngineContainer()
-        Assert.assertEquals("Fuel", fuelEngineContainer.toString())
+        assertEquals("Fuel", fuelEngineContainer.toString())
+    }
+
+    @Test
+    fun `setting connect timeout for not null`() {
+        val client = HttpClient {
+            install(HttpTimeout) {
+                connectTimeoutMillis = 3000
+            }
+        }
+        assertNotNull(client)
+    }
+
+    @Test
+    fun `setting request timeout for not null`() = runBlocking {
+        val client = HttpClient {
+            install(HttpTimeout)
+        }
+        val response = client.get<String>("https://www.google.com") {
+            timeout {
+                requestTimeoutMillis = 1300
+            }
+        }
+        assertNotNull(response)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `throws error for HttpTimeout Feature not installed`() = runBlocking {
+        val client = HttpClient()
+        client.get<String>("https://www.google.com") {
+            timeout {
+
+            }
+        }
+        assertNotNull(client)
+    }
+
+    @Test
+    fun `setting socket timeout for not null`() {
+        val client = HttpClient {
+            install(HttpTimeout) {
+                socketTimeoutMillis = 1500
+            }
+        }
+        assertNotNull(client)
     }
 }
