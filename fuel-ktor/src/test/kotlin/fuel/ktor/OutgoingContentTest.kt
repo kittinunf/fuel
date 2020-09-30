c6700ea526b6ff97527f515d08a2feb617b26078
iNoles <xfsunoles@gmail.com>
2020-09-30 13:47:14 -0400

    Added Test for UnsupportedContentTypeException in OutgoingContentTest.kt

--- fuel-ktor/src/test/kotlin/fuel/ktor/OutgoingContentTest.kt
+++ fuel-ktor/src/test/kotlin/fuel/ktor/OutgoingContentTest.kt
@@ -1,15 +1,20 @@
 package fuel.ktor
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.UnsupportedContentTypeException
 import io.ktor.client.request.post
 import io.ktor.client.utils.EmptyContent
 import io.ktor.http.content.OutgoingContent
+import io.ktor.util.KtorExperimentalAPI
+import io.ktor.utils.io.ByteReadChannel
 import io.ktor.utils.io.ByteWriteChannel
 import io.ktor.utils.io.close
 import io.ktor.utils.io.writeStringUtf8
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertNotNull
 import org.junit.Test
+import kotlin.coroutines.CoroutineContext
 
 class OutgoingContentTest {
     @Test
@@ -39,6 +44,25 @@ class OutgoingContentTest {
                 override suspend fun writeTo(channel: ByteWriteChannel) {
                     channel.writeStringUtf8(content)
                     channel.close()
+                }
+            }
+        }
+        assertNotNull(response)
+    }
+
+    @KtorExperimentalAPI
+    @Test(expected = UnsupportedContentTypeException::class)
+    fun `should throw Error over protocol upgrade`() = runBlocking {
+        val client = HttpClient()
+        val response = client.post<String>("http://httpbin.org/post") {
+            body = object : OutgoingContent.ProtocolUpgrade() {
+                override suspend fun upgrade(
+                    input: ByteReadChannel,
+                    output: ByteWriteChannel,
+                    engineContext: CoroutineContext,
+                    userContext: CoroutineContext
+                ): Job {
+                    throw UnsupportedContentTypeException(this)
                 }
             }
         }
