00f2d7ff0b78d2ed10eac9e8bf9b715fce32206c
iNoles <xfsunoles@gmail.com>
2020-10-26 16:34:25 -0400

    Updated some Kotlin libraries. Forget to update Kotlin version in README.md

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -1,27 +1,27 @@
 object Library {
     const val KOTLIN_VERSION = "1.4.10"
 
-    private const val COROUTINES_VERSION = "1.3.9"
+    private const val COROUTINES_VERSION = "1.4.0"
     const val KOTLINX_COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$COROUTINES_VERSION"
 
     private const val OKHTTP_VERSION = "4.10.0-RC1"
     const val OKHTTP = "com.squareup.okhttp3:okhttp:$OKHTTP_VERSION"
     const val OKHTTP_MOCK_WEB_SERVER = "com.squareup.okhttp3:mockwebserver:$OKHTTP_VERSION"
 
-    private const val FORGE_VERSION = "1.0.0-alpha2"
+    private const val FORGE_VERSION = "1.0.0-alpha3"
     const val FORGE = "com.github.kittinunf.forge:forge:$FORGE_VERSION"
 
     private const val SERIALIZATION_VERSION = "1.0.0"
     const val KOTLINX_SERIALIZATION_RUNTIME = "org.jetbrains.kotlinx:kotlinx-serialization-json:$SERIALIZATION_VERSION"
 
-    private const val MOSHI_VERSION = "1.10.0"
+    private const val MOSHI_VERSION = "1.11.0"
     const val MOSHI = "com.squareup.moshi:moshi:$MOSHI_VERSION"
     const val MOSHI_KOTLIN_CODEGEN = "com.squareup.moshi:moshi-kotlin-codegen:$MOSHI_VERSION"
 
-    private const val KTOR_VERSION = "1.4.0"
+    private const val KTOR_VERSION = "1.4.1"
     const val KTOR_CLIENT_JVM = "io.ktor:ktor-client-core-jvm:$KTOR_VERSION"
 
-    private const val JACKSON_VERSION = "2.11.2"
+    private const val JACKSON_VERSION = "2.12.0-rc1"
     const val JACKSON_KOTLIN_MODULE = "com.fasterxml.jackson.module:jackson-module-kotlin:$JACKSON_VERSION"
 
     const val JUNIT = "junit:junit:4.13"
