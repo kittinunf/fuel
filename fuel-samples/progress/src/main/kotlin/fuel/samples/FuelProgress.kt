4d606137f372b65ee851c8807f09c61e96fd6943
Kittinun Vantasin <kittinun.f@gmail.com>
2021-02-22 15:53:18 +0900

    Fix indentation for our lint

--- fuel-samples/progress/src/main/kotlin/fuel/samples/FuelProgress.kt
+++ fuel-samples/progress/src/main/kotlin/fuel/samples/FuelProgress.kt
@@ -33,12 +33,12 @@ val progressListener = object : ProgressListener {
 fun main() {
     runBlocking {
         val client = OkHttpClient.Builder()
-                .addNetworkInterceptor {
-                    val originalResponse = it.proceed(it.request())
-                    originalResponse.newBuilder()
-                            .body(ProgressResponseBody(originalResponse.body!!, progressListener))
-                            .build()
-                }.build()
+            .addNetworkInterceptor {
+                val originalResponse = it.proceed(it.request())
+                originalResponse.newBuilder()
+                    .body(ProgressResponseBody(originalResponse.body!!, progressListener))
+                    .build()
+            }.build()
         val httpLoader = HttpLoader.Builder().okHttpClient(client).build()
         val request = Request.Builder().data("https://publicobject.com/helloworld.txt").build()
         val string = httpLoader.get(request).body!!.string()
