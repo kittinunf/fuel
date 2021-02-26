290e98ae7965bfab093e3ba1394117bcc2b39193
iNoles <xfsunoles@gmail.com>
2021-02-25 23:13:20 -0500

    oh silly whitespaces

--- fuel-jackson/build.gradle.kts
+++ fuel-jackson/build.gradle.kts
@@ -4,7 +4,7 @@ val okhttpVersion: String by extra
 
 dependencies {
     api(project(":fuel-base"))
-    api( "com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
+    api("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
 
     testImplementation(project(":fuel-singleton"))
     testImplementation("junit:junit:4.13")
