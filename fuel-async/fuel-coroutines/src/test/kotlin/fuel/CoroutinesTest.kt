717c120ffa77ac2ab41b08b4733453f589f8a9d2
Jonathan <iNoles@users.noreply.github.com>
2021-03-11 23:39:23 -0500

    Support Fuel-Async Related Libraries (#799)
    * Support Fuel-Async Related Libraries
    
    * add newline at end of the file
    
    * added Single.doOnDispose with a test
    
    * added more tests
    
    * added newline on Mono Test. Moved doOnDispose to another lines.
    
    * Now using create instead defer for setCancellable
    
    * Hmm, wondering why this code works
    
    * Damn typos!!!
    
    * 100% code coverages back to Fuel-Async
    
    * Fix call rxjava implementation
    
    * Fix test
    
    * added assertTrue for isDisposed
    
    * One more try for isDisposed checks.
    
    Co-authored-by: Kittinun Vantasin <kittinun.f@gmail.com>

---
+++ fuel-async/fuel-coroutines/src/test/kotlin/fuel/CoroutinesTest.kt
@@ -0,0 +1,51 @@
+package fuel
+
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import mockwebserver3.MockResponse
+import mockwebserver3.MockWebServer
+import mockwebserver3.SocketPolicy
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+internal class CoroutinesTest {
+
+    private lateinit var mockWebServer: MockWebServer
+
+    @Before
+    fun `before test`() {
+        mockWebServer = MockWebServer()
+        mockWebServer.start()
+    }
+
+    @After
+    fun `after test`() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun `get with url`() = runBlocking {
+        mockWebServer.enqueue(MockResponse().setBody("Hello Get"))
+
+        val request = Request.Builder().data(mockWebServer.url("url")).build()
+        val response = HttpLoader().get(request).toCoroutines()
+        assertEquals("Hello Get", response.body!!.string())
+    }
+
+    @Test
+    fun `no socket response error`() = runBlocking {
+        mockWebServer.enqueue(MockResponse().setSocketPolicy(SocketPolicy.NO_RESPONSE))
+
+        val request = Request.Builder().data(mockWebServer.url("error")).build()
+        val job = launch {
+            HttpLoader().get(request).toCoroutines()
+        }
+
+        delay(500)
+        job.cancelAndJoin()
+    }
+}
