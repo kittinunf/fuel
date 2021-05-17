1efe5fecd624c9f046b20836d00ac61185a98237
Jonathan <iNoles@users.noreply.github.com>
2021-05-17 11:25:11 -0400

    Update Kotlin and Jacoco Version (#808)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.31"
-    kotlin("kapt") version "1.4.31" apply false
+    kotlin("jvm") version "1.5.0"
+    kotlin("kapt") version "1.5.0" apply false
     id("com.vanniktech.maven.publish") version "0.10.0" apply false
     jacoco
 }
@@ -14,7 +14,7 @@ allprojects {
     apply(plugin = "jacoco")
 
     val okhttpVersion by extra("5.0.0-alpha.2")
-    val moshiVersion by extra("1.11.0")
+    val moshiVersion by extra("1.12.0")
 
     repositories {
         mavenCentral()
@@ -40,7 +40,7 @@ allprojects {
     }
 
     jacoco {
-        toolVersion = "0.8.6"
+        toolVersion = "0.8.7"
     }
 
     tasks.jacocoTestReport {
