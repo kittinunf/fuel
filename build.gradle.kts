93a621654f70bdb9d8255afac59ca292be5ea54f
Kittinun Vantasin <kittinun.f@gmail.com>
2023-04-02 11:09:37 +0900

    [Publish] Add github action for publish to maven (#852)
    * Add github action to publish
    
    * Update version gradle
    
    * Update version for fuel
    
    * Update gradle and wrapper
    
    * Maybe we don't have to setup gradle alone
    
    * Update pr.yml
    
    * Update gradle.properties to increase memory
    
    * put back PR where Kittinunf had before
    
    * Update pr.yml
    
    ---------
    
    Co-authored-by: Jonathan <iNoles@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     kotlin("multiplatform") version "1.8.10" apply false
 }
 
+val isReleaseBuild: Boolean
+    get() = properties.containsKey("release")
+
 allprojects {
     repositories {
         mavenCentral()
@@ -9,5 +12,5 @@ allprojects {
     val artifactPublishVersion: String by project
     val artifactGroupId: String by project
     group = artifactGroupId
-    version = artifactPublishVersion
+    version = if (isReleaseBuild) artifactPublishVersion else "main-SNAPSHOT"
 }
