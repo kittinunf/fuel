555db5e51da28a9ffc1ad1b67a7c2219e5a8301f
Jonathan <iNoles@users.noreply.github.com>
2023-05-22 08:42:37 -0400

    Increasing Test Coverages (#866)

--- fuel-kotlinx-serialization/src/jvmTest/kotlin/fuel/serialization/FuelKotlinxSerializationTest.kt
+++ fuel-kotlinx-serialization/src/jvmTest/kotlin/fuel/serialization/FuelKotlinxSerializationTest.kt
@@ -32,4 +32,22 @@ class FuelKotlinxSerializationTest {
 
         mockWebServer.shutdown()
     }
+
+    @Test
+    fun testSerializableResponseWithDefaultJson() = runBlocking {
+        val mockWebServer = MockWebServer().apply {
+            enqueue(MockResponse().setBody("{\"userAgent\": \"Fuel2\"}"))
+            start()
+        }
+
+        val response = Fuel.get(mockWebServer.url("user-agent").toString())
+        val json = response.toJson(deserializationStrategy = HttpBinUserAgentModel.serializer())
+        json.fold({
+            assertEquals("Fuel2", it?.userAgent)
+        }, {
+            fail(it.message)
+        })
+
+        mockWebServer.shutdown()
+    }
 }
