8543da2315d4c76d905d452db833b8c7c2c1308e
Jonathan Steele <iNoles@users.noreply.github.com>
2023-11-12 22:27:27 -0500

    Upgrade some dependencies

--- fuel-kotlinx-serialization/build.gradle.kts
+++ fuel-kotlinx-serialization/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform")
-    kotlin("plugin.serialization") version "1.9.0"
+    kotlin("plugin.serialization") version "1.9.20"
     id("publication")
     id("org.jetbrains.kotlinx.kover")
 }
@@ -14,11 +14,11 @@ kotlin {
             useJUnit()
         }
     }
-    js(IR) {
+    js {
         browser()
         binaries.executable()
     }
-    ios {
+    iosArm64 {
         binaries {
             framework {
                 baseName = "Fuel-Serialization"
@@ -26,6 +26,13 @@ kotlin {
         }
     }
     macosArm64 {
+        binaries {
+            framework {
+                baseName = "Fuel-Serialization"
+            }
+        }
+    }
+    iosX64 {
         binaries {
             framework {
                 baseName = "Fuel-Serialization"
@@ -46,61 +53,26 @@ kotlin {
             }
         }
     }
+
     explicitApi()
+
     sourceSets {
-        val commonMain by getting {
+        commonMain {
             dependencies {
                 api(project(":fuel"))
                 api(libs.kotlinx.serialization.json)
                 api(libs.result)
             }
         }
-        val commonTest by getting {
+        commonTest {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val jvmMain by getting
-        val jvmTest by getting {
+        jvmTest {
             dependencies {
                 implementation(libs.mockwebserver)
             }
-        }
-        val jsMain by getting
-        val jsTest by getting
-
-        val appleMain by creating {
-            dependsOn(commonMain)
-        }
-
-        val iosMain by getting {
-            dependsOn(appleMain)
-        }
-        val macosArm64Main by getting {
-            dependsOn(appleMain)
-        }
-        val macosX64Main by getting {
-            dependsOn(appleMain)
-        }
-        val iosSimulatorArm64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val appleTest by creating {
-            dependsOn(commonTest)
-        }
-
-        val iosTest by getting {
-            dependsOn(appleTest)
-        }
-        val macosArm64Test by getting {
-            dependsOn(appleTest)
-        }
-        val macosX64Test by getting {
-            dependsOn(appleTest)
-        }
-        val iosSimulatorArm64Test by getting {
-            dependsOn(appleTest)
         }
     }
 }
