bb7d3b94e9fac7dbca442a7bba24ba69202e322d
Jonathan Steele <iNoles@users.noreply.github.com>
2024-01-15 21:36:22 -0500

    Upgrade some dependencies

--- fuel-moshi-jvm/build.gradle.kts
+++ fuel-moshi-jvm/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm")
     id("publication")
     id("org.jetbrains.kotlinx.kover")
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16"
 }
 
 kotlin {
