2b14c3befe6d12d91d0459a42167edd0725f747e
Jonathan <iNoles@users.noreply.github.com>
2024-01-22 00:57:37 -0500

    Upgrade some Dependencies (#882)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.9.20" apply false
-    id("org.jetbrains.kotlinx.kover") version "0.7.4" apply false
+    kotlin("multiplatform") version "1.9.21" apply false
+    id("org.jetbrains.kotlinx.kover") version "0.7.5" apply false
 }
 
 val isReleaseBuild: Boolean
