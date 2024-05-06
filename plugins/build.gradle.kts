c75d7aae4cb3e0cb1292946086f532b6318e2418
Jonathan Steele <iNoles@users.noreply.github.com>
2024-05-05 23:59:49 -0400

    Update build.gradle.kts

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -6,10 +6,6 @@ repositories {
     gradlePluginPortal()
 }
 
-kotlin {
-    jvmToolchain(17)
-}
-
 java {
     sourceCompatibility = JavaVersion.VERSION_11_7
     targetCompatibility = JavaVersion.VERSION_11_7
