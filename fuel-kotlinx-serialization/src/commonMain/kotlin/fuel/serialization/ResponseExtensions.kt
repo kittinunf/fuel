d092569ec9cbf57f4d567f87630507f9ac6a09b8
Jonathan Steele <iNoles@users.noreply.github.com>
2024-05-28 10:47:28 -0400

    Oops, how i forget to check this. :(

--- fuel-kotlinx-serialization/src/commonMain/kotlin/fuel/serialization/ResponseExtensions.kt
+++ fuel-kotlinx-serialization/src/commonMain/kotlin/fuel/serialization/ResponseExtensions.kt
@@ -1,14 +1,11 @@
 package fuel.serialization
 
 import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.runCatching
 import fuel.HttpResponse
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.json.Json
 
-public actual fun <T : Any> HttpResponse.toJson(
-    json: Json,
+public expect fun <T : Any> HttpResponse.toJson(
+    json: Json = Json { allowStructuredMapKeys = true },
     deserializationStrategy: DeserializationStrategy<T>
-): Result<T?, Throwable> = runCatching {
-    body?.let { json.decodeFromString(deserializationStrategy, it) }
-}
+): Result<T?, Throwable>
