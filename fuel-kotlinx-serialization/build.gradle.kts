952f501865b426d1711538516822d79ced499b01
Jonathan Steele <iNoles@users.noreply.github.com>
2024-06-17 17:31:02 -0400

    Update build.gradle.kts

--- fuel-kotlinx-serialization/build.gradle.kts
+++ fuel-kotlinx-serialization/build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
     kotlin("multiplatform")
